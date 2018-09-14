05043e2489c6157ca6885c6a2e6bb440c24c2ec2
Patrick Simonian <Patrick.Simonian@gov.bc.ca>
2018-09-14 13:48:10 -0700

    split up mocks for gatsby, gatsby-link mock is now unmocked

--- src/__mocks__/gatsby-link.js
+++ src/__mocks__/gatsby-link.js
@@ -1,4 +1,4 @@
-// this is a work around to allow of actual use of Link component 
+// this is a work around to allow of actual use of Link component
 // https://www.gatsbyjs.org/docs/unit-testing/#testing-components-with-router
-jest.unmock("gatsby");
-module.exports = jest.requireActual("gatsby-link");
+jest.unmock('gatsby');
+module.exports = jest.requireActual('gatsby-link');
--- src/__mocks__/gatsby.js
+++ src/__mocks__/gatsby.js
@@ -1,2 +1,2 @@
-const gatsby = jest.requireActual("gatsby")
-module.exports = { ...gatsby, graphql: jest.fn() }
+const gatsby = jest.requireActual('gatsby');
+module.exports = { ...gatsby, graphql: jest.fn() };
