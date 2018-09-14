8530811d31862842a7e0c9f01718257d7c9baad2
Patrick Simonian <Patrick.Simonian@gov.bc.ca>
2018-09-14 10:35:30 -0700

    add more placeholder content to landing page

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -2,10 +2,13 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Helmet from 'react-helmet';
 /* eslint-disable */
+// font awesome loading
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faCheckSquare, faCoffee } from '@fortawesome/free-solid-svg-icons';
+library.add(faCheckSquare, faCoffee);
 import normalize from 'normalize.css';
 import '../assets/styles/index.css';
 import '../assets/styles/fonts.css';
-
 import PrimaryFooter from '../components/PrimaryFooter/PrimaryFooter';
 import PrimaryHeader from '../components/PrimaryHeader/PrimaryHeader';
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,5 +2,21 @@
 import React from 'react';
 import NavigationalItems from '../components/Navigation/NavigationalItems/NavigationalItems';
 
-const links = ['blah', 'blah', 'blah', 'baashasdf', 'asdf'];
-export default () => <NavigationalItems navItems={links} />;
+const links = ['Learn', 'Do', 'Deliver'];
+
+export default () => (
+  <main role="main" className="container">
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque
+      imperdiet ullamcorper massa, eget venenatis mauris sagittis ut. Nunc
+      rutrum libero consequat turpis faucibus semper. In hac habitasse platea
+      dictumst. Vivamus vel imperdiet lacus, quis euismod sapien. Vivamus quis
+      volutpat lorem. Ut in turpis nec erat interdum interdum. Interdum et
+      malesuada fames ac ante ipsum primis in faucibus. Nam sagittis id ipsum
+      vitae semper. Aenean sit amet euismod leo. Curabitur nec erat nisi. Ut
+      sapien lorem, consequat molestie dignissim vel, maximus vel metus. Mauris
+      at gravida neque.
+    </p>
+    <NavigationalItems navItems={links} />
+  </main>
+);
