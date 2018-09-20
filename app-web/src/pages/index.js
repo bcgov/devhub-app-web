ff47bdafdca0318942681e36aaca009ee279f5e3
Patrick Simonian <Patrick.Simonian@gov.bc.ca>
2018-09-20 13:45:50 -0700

    refactor styling into a global style sheet for pages, also refactored routes into its own 'config' file for reusability

--- app-web/src/pages/index.js
+++ app-web/src/pages/index.js
@@ -1,52 +1,30 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import classes from './Index.module.css';
 // local components
 import NavigationalItems from '../components/Navigation/NavigationalItems/NavigationalItems';
-import { LEARN_ROUTE, DO_ROUTE, DELIVER_ROUTE } from '../constants/routes';
-
-const links = [
-  {
-    link: LEARN_ROUTE,
-    title: 'Learn',
-    icon: 'book',
-    description: 'Learn more about the Pathfinder initiative',
-  },
-  {
-    link: DO_ROUTE,
-    title: 'Do',
-    icon: 'user-astronaut',
-    description: 'Use the Signing Tool',
-  },
-  {
-    link: DELIVER_ROUTE,
-    title: 'Deliver',
-    icon: 'shipping-fast',
-    description:
-      'this is a description describing what this block is all about',
-  },
-];
+import links from '../mainNavigation';
 
 class Index extends Component {
   render() {
+    console.log(links);
     return (
-      <main role="main" className={classes.Main}>
+      <main role="main" className="main">
         <h1>Welcome</h1>
         <h2>
           <em>We are here to help</em>
         </h2>
-        <p className={classes.Para}>
+        <p className="para">
           This is the front door to the developer community of the BC
           Government. We’re embarking on a journey to help developers learn new
           skills, discover resources and create amazing applications for
           government.
         </p>
-        <p className={classes.Para}>
+        <p className="para">
           This is our first release and we’re planning to have new material
           added to this site every sprint (2 weeks), so please visit often to
           check our progress.
         </p>
-        <p className={classes.Para}>
+        <p className="para">
           {' '}
           If you’d like to comment, offer a suggestion or ask a question you can
           find us by opening an issue in our github.com repository.
