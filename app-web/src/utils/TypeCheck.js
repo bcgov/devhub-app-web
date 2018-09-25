e07ea598960c581627823074961cb0f33ffc8688
Patrick Simonian <Patrick.Simonian@gov.bc.ca>
2018-09-25 14:37:36 -0700

    Add a typechecking utility library
    Type checking is painful, this helper class contains predicatable cross browser type checking

---
+++ app-web/src/utils/TypeCheck.js
@@ -0,0 +1,33 @@
+export class TypeCheck {
+    static getClass(object) {
+        return Object.prototype.toString.call(object).slice(8, -1);
+    }
+
+    static isArray(object) {
+        return TypeCheck.getClass(object) === 'Array';
+    }
+
+    static isObject(object) {
+        return TypeCheck.getClass(object) === 'Object';
+    }
+
+    static isBoolean(object) {
+        return TypeCheck.getClass(object) === 'Boolean';
+    }
+
+    static isNumber(object) {
+        return TypeCheck.getClass(object) === 'Number';
+    }
+
+    static isString(object) {
+        return TypeCheck.getClass(object) === 'String';
+    }
+
+    static isDate(object) {
+        return TypeCheck.getClass(object) === 'Date';
+    }
+
+    static isRegExp(object) {
+        return TypeCheck.getClass(object) === 'RegExp';
+    }
+}
