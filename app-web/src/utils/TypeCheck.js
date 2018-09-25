aa1378e822a81e3244421f993af184cec4f1de9e
Patrick Simonian <Patrick.Simonian@gov.bc.ca>
2018-09-25 16:05:09 -0700

    Add some docs to TypeCheck.js

--- app-web/src/utils/TypeCheck.js
+++ app-web/src/utils/TypeCheck.js
@@ -1,3 +1,8 @@
+/**
+ * A dependable Type Checking Utility
+ * Type checking using the native typeof or instanceof can cause issues at times
+ * as explained by https://juhukinners.wordpress.com/2009/01/11/typeof-considered-useless-or-how-to-write-robust-type-checks/
+ */
 export class TypeCheck {
   static getClass(object) {
     return Object.prototype.toString.call(object).slice(8, -1);
