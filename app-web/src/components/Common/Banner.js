13a372f77aa67c3a84f6a4e38b804eb0b0edd036
Patrick Simonian <Patrick.Simonian@gov.bc.ca>
2018-09-26 16:44:07 -0700

    update banner wrapping component to be a div and navigate on click via event

--- app-web/src/components/Common/Banner.js
+++ app-web/src/components/Common/Banner.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import Link from 'gatsby-link';
-import { HOME_ROUTE } from '../../constants/routes';
+import { navigateTo } from 'gatsby-link';
+import { BANNER_ID } from '../../constants/ui';
 import GovLogo from '../UI/GovLogo/GovLogo';
 import classes from './Common.module.css';
 
 const Banner = ({ title }) => {
   return (
-    <Link to={HOME_ROUTE} className={classes.Logo}>
+    <div onClick={() => {
+      navigateTo('/');
+    }} className={classes.Logo}>
       <GovLogo />
       <h1>{title}</h1>
-    </Link>
+    </div>
   );
 };
 
--- app-web/src/constants/ui.js
+++ app-web/src/constants/ui.js
@@ -3,3 +3,4 @@
 export const buttonTypes = ['primary', 'secondary', 'link'];
 export const LOGIN_BTN_ID = 'dh-login';
 export const LOGOUT_BTN_ID = 'dh-logout';
+export const BANNER_ID = 'dh-banner';
