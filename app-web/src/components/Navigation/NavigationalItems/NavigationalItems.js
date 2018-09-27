9095e177dc9cd53c6ef414ab627be13b1ca6f651
Patrick Simonian <Patrick.Simonian@gov.bc.ca>
2018-09-27 13:56:43 -0700

    adjust hex block to allow for ids to be passed in and nav items now contain ids for bdd testing

--- app-web/src/components/Navigation/NavigationalItems/NavigationalItems.js
+++ app-web/src/components/Navigation/NavigationalItems/NavigationalItems.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import NavigationalItem from './NavigationalItem/NavigationalItem';
 import shortid from 'shortid';
+import { MAIN_NAVIGATION_BTN } from '../../../constants/ui';
+import NavigationalItem from './NavigationalItem/NavigationalItem';
 import Hexgrid from '../../UI/Hexgrid/Hexgrid';
 import classes from './NavigationalItems.module.css';
 
 const NavigationalItems = props => {
   //get class name mappins so that the navigational item hexes display as a grid
   const navItems = props.navItems.map((item, ind) => (
-    <NavigationalItem key={shortid.generate()} {...item} />
+    <NavigationalItem key={shortid.generate()} {...item} id={`${MAIN_NAVIGATION_BTN}-${ind}`}/>
   ));
 
   return (
--- app-web/src/constants/ui.js
+++ app-web/src/constants/ui.js
@@ -4,3 +4,4 @@ export const buttonTypes = ['primary', 'secondary', 'link'];
 export const LOGIN_BTN_ID = 'dh-login';
 export const LOGOUT_BTN_ID = 'dh-logout';
 export const BANNER_ID = 'dh-banner';
+export const MAIN_NAVIGATION_BTN = 'dh-main-nav';
