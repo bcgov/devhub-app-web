711f585b5f6697c61285ab12875f5d134b847573
ShellyXueHan <shellshell456@gmail.com>
2018-09-27 12:06:11 -0700

    update bdd configs

--- pipeline/src/groovy/BddStack.groovy
+++ pipeline/src/groovy/BddStack.groovy
@@ -10,10 +10,10 @@ File scriptSourceFile = Paths.get(scriptSourceUri).toFile()
 cli = new CliBuilder(usage: "groovy ${scriptSourceFile.getName()} --pr=<pull request#>")
 
 cli.with {
-    h(longOpt: 'help', 'Show usage information')
-    c(longOpt: 'config', args: 1, argName: 'Pipeline config file', 'Pipeline config file', required: true)
-    e(longOpt: 'env', args: 1, argName: 'Target environment name', 'Target environment name', required: true)
-    _(longOpt: 'pr', args: 1, argName: 'Pull Request Number', 'GitHub Pull Request #', required: true)
+  h(longOpt: 'help', 'Show usage information')
+  c(longOpt: 'config', args: 1, argName: 'Pipeline config file', 'Pipeline config file', required: true)
+  e(longOpt: 'env', args: 1, argName: 'Target environment name', 'Target environment name', required: true)
+  _(longOpt: 'pr', args: 1, argName: 'Pull Request Number', 'GitHub Pull Request #', required: true)
 }
 
 
@@ -21,27 +21,27 @@ opt = cli.parse(args)
 
 
 if (opt == null) {
-    //System.err << 'Error while parsing command-line options.\n'
-    //cli.usage()
-    System.exit 2
+  //System.err << 'Error while parsing command-line options.\n'
+  //cli.usage()
+  System.exit 2
 }
 
 if (opt?.h) {
-    cli.usage()
-    return 0
+  cli.usage()
+  return 0
 }
 
 def config = OpenShiftHelper.loadDeploymentConfig(opt)
 def toolsNamespace = "devhub-tools"
 
 def appLabel="${config.app.deployment.id}"
-def routes = ocGet(['routes','-l', "app=${appLabel},component=mds-frontend", "--namespace=${config.app.deployment.namespace}"])
+def routes = ocGet(['routes','-l', "app=${appLabel}", "--namespace=${config.app.deployment.namespace}"])
 
 routes.items.each {Map route ->
-    String routeProtocol = ((route.spec?.tls!=null)?'https':'http')
-    String routeUrl = "${routeProtocol}://${route.spec.host}${route.spec.path?:'/'}"
-    println "URLs found:  ${routeUrl}"
-    OpenShiftHelper._exec(["bash", '-c', "oc process -f openshift/bddstack.pod.json -l 'bdd=${route.metadata.name}' -l 'app-name=${config.app.name}' -l 'app=${appLabel}' -p 'NAME=bdd-stack' -p 'URL=${routeUrl}' -p 'SUFFIX=${config.app.build.suffix}' -p 'VERSION=${config.app.build.version}' --namespace='${toolsNamespace}' |  oc replace -f - --namespace='${toolsNamespace}' --force=true"], new StringBuffer(), new StringBuffer())
+  String routeProtocol = ((route.spec?.tls!=null)?'https':'http')
+  String routeUrl = "${routeProtocol}://${route.spec.host}${route.spec.path?:'/'}"
+  println "URLs found:  ${routeUrl}"
+  OpenShiftHelper._exec(["bash", '-c', "oc process -f openshift/bddstack.pod.json -l 'bdd=${route.metadata.name}' -l 'app-name=${config.app.name}' -l 'app=${appLabel}' -p 'NAME=bdd-stack' -p 'URL=${routeUrl}' -p 'SUFFIX=${config.app.build.suffix}' -p 'VERSION=${config.app.build.version}' --namespace='${toolsNamespace}' |  oc replace -f - --namespace='${toolsNamespace}' --force=true"], new StringBuffer(), new StringBuffer())
 }
 
 int inprogress=1
@@ -49,18 +49,18 @@ boolean hasFailed=false;
 
 
 while(inprogress>0){
-    Map pods = ocGet(['pods','-l', "app=${appLabel}", "--namespace=${toolsNamespace}"])
-    inprogress=0
-    for (Map pod:pods.items){
-        if ('Failed' == pod.status.phase) {
-            hasFailed = true
-            continue
-        }
-        if ('Succeeded' == pod.status.phase) continue
-        println "Waiting for '${pod.metadata.name}' (${pod.status.phase})"
-        inprogress++
+  Map pods = ocGet(['pods','-l', "app=${appLabel}", "--namespace=${toolsNamespace}"])
+  inprogress=0
+  for (Map pod:pods.items){
+    if ('Failed' == pod.status.phase) {
+      hasFailed = true
+      continue
     }
-    Thread.sleep(2000)
+    if ('Succeeded' == pod.status.phase) continue
+    println "Waiting for '${pod.metadata.name}' (${pod.status.phase})"
+    inprogress++
+  }
+  Thread.sleep(2000)
 }
 
 if (hasFailed) System.exit(1)
