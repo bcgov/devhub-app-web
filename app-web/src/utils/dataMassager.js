0d2b0ec4f7ab394716365bafdadeb5ab7bf6bd82
Patrick Simonian <Patrick.Simonian@gov.bc.ca>
2018-10-04 15:21:50 -0700

    add a group by function

---
+++ app-web/src/utils/dataMassager.js
@@ -0,0 +1,38 @@
+// group array of objects by a key
+// arr = [ {foo: "bar", fruit: "apple"}, {foo: "baer", fruit: "banana"}]
+// groupBy(arr, "fruit") => [{fruit: "apple", data: {foo: "bar", fruit: "apple"}} ..etc]
+import { TypeCheck } from '@bcgov/common-web-utils';
+/**
+ * @param {Array} collection
+ * @param {String} key
+ * @returns {Array}
+ */
+// eslint-disable-next-line
+export const groupBy = (collection, key) => {
+  if (!TypeCheck.isArray(collection)) {
+    throw new Error('Collection must be type of Array');
+  }
+
+  if (!key || !TypeCheck.isString(key)) {
+    throw new Error('Key must be type of String');
+  }
+
+  const grouping = {};
+  collection.forEach(c => {
+    if (!TypeCheck.isObject(c)) {
+      throw new Error('Collection must only contain Objects');
+    }
+    if (c[key]) {
+      if (!grouping[c[key]]) {
+        grouping[c[key]] = [c];
+      } else {
+        grouping[c[key]].push(c);
+      }
+    }
+  });
+
+  return Object.keys(grouping).map(groupKey => ({
+    [key]: groupKey,
+    data: grouping[groupKey],
+  }));
+};
--- app-web/src/utils/utils.test.js
+++ app-web/src/utils/utils.test.js
@@ -1,4 +1,6 @@
 import hexGridCalculator from './hexGridCalculator';
+import { groupBy } from './dataMassager';
+
 describe('Hex Grid Calculation', () => {
   test('it should return [3, 3, 3, 2, 2] when passed in 5 and [3, 2]', () => {
     const expected = [3, 3, 3, 2, 2];
@@ -31,5 +33,149 @@ describe('Hex Grid Calculation', () => {
     const arrayLength = 5;
     const pattern = [-1, 3];
     expect(() => hexGridCalculator(arrayLength, pattern)).toThrow();
+  });
+});
+
+describe('Data Massagers', () => {
+  test('it groups data correctly when passed in the right arguments', () => {
+    const data = [
+      {
+        foo: 'bar',
+        name: 'banana',
+        fruit: 'something',
+        category: 'fruit',
+      },
+      {
+        foo: 'bar',
+        name: 'apple',
+        fruit: 'something',
+        category: 'fruit',
+      },
+      {
+        foo: 'bar',
+        name: 'hammer',
+        category: 'tools',
+      },
+      {
+        foo: 'bar',
+        name: 'wrench',
+        fruit: 'something',
+        category: 'tools',
+      },
+    ];
+
+    const expected = [
+      {
+        category: 'fruit',
+        data: [
+          {
+            foo: 'bar',
+            name: 'banana',
+            fruit: 'something',
+            category: 'fruit',
+          },
+          {
+            foo: 'bar',
+            name: 'apple',
+            fruit: 'something',
+            category: 'fruit',
+          },
+        ],
+      },
+      {
+        category: 'tools',
+        data: [
+          {
+            foo: 'bar',
+            name: 'hammer',
+            category: 'tools',
+          },
+          {
+            foo: 'bar',
+            name: 'wrench',
+            fruit: 'something',
+            category: 'tools',
+          },
+        ],
+      },
+    ];
+    expect(groupBy(data, 'category')).toEqual(expected);
+  });
+
+  test('Throws error if key is not passed into func', () => {
+    expect(() => {
+      groupBy([{ a: true }]);
+    }).toThrow('Key must be type of String');
+  });
+
+  test('Throws if collection passed is not an array', () => {
+    expect(() => {
+      groupBy(123);
+    }).toThrow('Collection must be type of Array');
+  });
+
+  test('Throws if collection is not array of objects', () => {
+    expect(() => {
+      groupBy([1, 2, 5], 'key');
+    }).toThrow('Collection must only contain Objects');
+  });
+
+  test("If grouping doesn't exist in object it is not returned", () => {
+    const data = [
+      {
+        foo: 'bar',
+        name: 'banana',
+        fruit: 'something',
+        category: 'fruit',
+      },
+      {
+        foo: 'bar',
+        name: 'apple',
+        fruit: 'something',
+        category: 'fruit',
+      },
+      {
+        foo: 'bar',
+        name: 'hammer',
+        category: 'tools',
+      },
+      {
+        foo: 'bar',
+        name: 'wrench',
+        fruit: 'something',
+      },
+    ];
+
+    const expected = [
+      {
+        category: 'fruit',
+        data: [
+          {
+            foo: 'bar',
+            name: 'banana',
+            fruit: 'something',
+            category: 'fruit',
+          },
+          {
+            foo: 'bar',
+            name: 'apple',
+            fruit: 'something',
+            category: 'fruit',
+          },
+        ],
+      },
+      {
+        category: 'tools',
+        data: [
+          {
+            foo: 'bar',
+            name: 'hammer',
+            category: 'tools',
+          },
+        ],
+      },
+    ];
+
+    expect(groupBy(data, 'category')).toEqual(expected);
   });
 });
