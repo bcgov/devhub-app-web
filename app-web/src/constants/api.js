28dcb0197af981db1a0662911aeee2599ddad3d5
Patrick Simonian <Patrick.Simonian@gov.bc.ca>
2018-10-09 11:27:22 -0700

    add default base url to dev

--- app-web/src/constants/api.js
+++ app-web/src/constants/api.js
@@ -1,3 +1,3 @@
 export const SSO_REALM_NAME = 'devhub';
 export const SSO_CLIENT_ID = 'devhub-web';
-export const SSO_BASE_URL = process.env.GATSBY_SSO_BASE_URL;
+export const SSO_BASE_URL = process.env.GATSBY_SSO_BASE_URL || 'https://sso-dev.pathfinder.gov.bc.ca';
