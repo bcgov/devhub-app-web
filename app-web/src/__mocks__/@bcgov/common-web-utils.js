05a26b2822bfa3f71116767c630e429a21ee1848
Patrick Simonian <Patrick.Simonian@gov.bc.ca>
2018-10-09 10:50:37 -0700

    mock out implicit auth manager for unit tests and fix broken unit test for learn page

---
+++ app-web/src/__mocks__/@bcgov/common-web-utils.js
@@ -0,0 +1,20 @@
+const { ImplicitAuthManager, TypeCheck } = jest.requireActual(
+  '@bcgov/common-web-utils'
+);
+
+class ImplicitAuthManagerMocked {
+  // eslint-disable-next-line
+  getSSOLoginURI() {
+    return 'LOGINURI';
+  }
+  // eslint-disable-next-line
+  getSSOLogoutURI() {
+    return 'LOGOUTURI';
+  }
+  // eslint-disable-next-line
+  handleOnPageLoad() {
+    return undefined;
+  }
+}
+
+module.exports = { ImplicitAuthManager: ImplicitAuthManagerMocked, TypeCheck };
