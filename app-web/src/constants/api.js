269d925089ced427ab61e211eb5f384e7827f88d
Patrick Simonian <Patrick.Simonian@gov.bc.ca>
2018-10-09 10:47:39 -0700

    move environment variables into a .production style file
    environment variables now loaded from prod file
    
    base url loaded from env

--- app-web/src/constants/api.js
+++ app-web/src/constants/api.js
@@ -1,4 +1,3 @@
-export const SSO_BASE_URL = 'https://sso-dev.pathfinder.gov.bc.ca';
 export const SSO_REALM_NAME = 'devhub';
-export const SSO_BASE_AUTH_ENDPOINT = `${SSO_BASE_URL}/auth/realms/${SSO_REALM_NAME}/protocol/openid-connect`;
 export const SSO_CLIENT_ID = 'devhub-web';
+export const SSO_BASE_URL = process.env.GATSBY_SSO_BASE_URL;
