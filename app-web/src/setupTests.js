bb0921d81d59a4e03a06b8d3e9da378549a0cb43
Patrick Simonian <Patrick.Simonian@gov.bc.ca>
2018-10-09 15:31:55 -0700

    Integrate authentication mechanics (#33)
    Merged PR-33

--- app-web/src/setupTests.js
+++ app-web/src/setupTests.js
@@ -2,4 +2,7 @@
 import Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
+// mock out graph ql
+global.graphql = jest.fn();
+
 export default Enzyme.configure({ adapter: new Adapter() });
