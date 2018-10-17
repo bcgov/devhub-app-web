2199edcd525c50f85eb0d339cdb52aa55f74d3ca
Patrick Simonian <Patrick.Simonian@gov.bc.ca>
2018-10-17 16:51:45 -0700

    refactored resource/item components

--- app-web/src/components/Resource/Item/Item.js
+++ app-web/src/components/Resource/Item/Item.js
@@ -2,26 +2,29 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './Item.module.css';
 
-const Item = ({ description, link, children }) => (
-  <div className={styles.Item}>
-    <h1>{description}</h1>
-    <div className={styles.Body}>{children}</div>
-    <div className={styles.Bottom}>
-      <a href={link} className={styles.Action}>
-        Learn more
-      </a>
+const Item = ({ title, abstract, link }) => {
+  const abstractPara = abstract ? (
+    <p>{abstract}</p>
+  ) : (
+    <p>Click below for more details.</p>
+  );
+  return (
+    <div className={styles.Item}>
+      <h1>{title}</h1>
+      <div className={styles.Body}>{abstractPara}</div>
+      <div className={styles.Bottom}>
+        <a href={link} className={styles.Action}>
+          Learn more
+        </a>
+      </div>
     </div>
-  </div>
-);
-
-Item.propTypes = {
-  children: PropTypes.string,
-  description: PropTypes.string.isRequired,
-  link: PropTypes.string.isRequired,
+  );
 };
 
-Item.defaultProps = {
-  children: '',
+Item.propTypes = {
+  title: PropTypes.string.isRequired,
+  abstract: PropTypes.string.isRequired,
+  link: PropTypes.string.isRequired,
 };
 
 export default Item;
