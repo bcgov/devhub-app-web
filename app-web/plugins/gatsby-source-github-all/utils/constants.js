d0c1208caa2c1b3a78a81e39274bae4410c701b8
Patrick Simonian <Patrick.Simonian@gov.bc.ca>
2018-10-17 09:54:44 -0700

    refactored constant into fetchFile and added export to constants.js

--- app-web/plugins/gatsby-source-github-all/utils/constants.js
+++ app-web/plugins/gatsby-source-github-all/utils/constants.js
@@ -23,11 +23,12 @@ const DEFUALT_IGNORES = [
   'CONTRIBUTING.md',
   'CODE_OF_CONDUCT.md',
   'LICENSE',
-  'CODE\ OF\ CONDUCT.md',
+  'CODE OF CONDUCT.md',
   'openshift',
 ];
 // github rest api v3
 const GITHUB_API_ENDPOINT = 'https://api.github.com';
+
 const FILETYPES = {
   yml: 'YAML',
   yaml: 'YAML',
@@ -47,6 +48,7 @@ const MEDIATYPES = {
 const PROCESSABLE_EXTENSIONS = ['.md'];
 
 module.exports = {
+  PROCESSABLE_EXTENSIONS,
   DEFUALT_IGNORES,
   GITHUB_API_ENDPOINT,
   FILETYPES,
