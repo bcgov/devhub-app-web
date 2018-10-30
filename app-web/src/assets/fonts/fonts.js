1c7d4af9008df3ac1d09e208a18c91b845a6e6a4
Patrick Simonian <Patrick.Simonian@gov.bc.ca>
2018-10-30 10:25:01 -0700

    add reset and font file as per design system header

--- app-web/src/assets/fonts/fonts.js
+++
@@ -1,5 +0,0 @@
-import MyriadWebProWoff from './MyriadWebPro.woff';
-import MyriadWebProWoff2 from './MyriadWebPro.woff2';
-import MyriadWebProTtf from './MyriadWebPro.ttf';
-
-export { MyriadWebProTtf, MyriadWebProWoff, MyriadWebProWoff2 };
