e4fe774e2587863a058ad3a733ad2f40dfc641cf
ShellyXueHan <shellshell456@gmail.com>
2018-10-31 12:26:34 -0700

    remove local env var

--- app-web/src/constants/api.js
+++ app-web/src/constants/api.js
@@ -1,6 +1,3 @@
-export const SSO_REALM_NAME =
-  process.env.GATSBY_SSO_REALM_NAME || '{{.Env.SSO_REALM_NAME}}';
-export const SSO_CLIENT_ID =
-  process.env.GATSBY_SSO_CLIENT_ID || '{{.Env.SSO_CLIENT_ID}}';
-export const SSO_BASE_URL =
-  process.env.GATSBY_SSO_BASE_URL || '{{.Env.SSO_BASE_URL}}';
+export const SSO_REALM_NAME = '{{.Env.SSO_REALM_NAME}}';
+export const SSO_CLIENT_ID = '{{.Env.SSO_CLIENT_ID}}';
+export const SSO_BASE_URL = '{{.Env.SSO_BASE_URL}}';
