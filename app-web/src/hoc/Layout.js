e5214856f3157e88fbbe9e571ae193db869466b3
ShellyXueHan <36056216+ShellyXueHan@users.noreply.github.com>
2018-10-31 16:46:51 -0700

    setup sso variables in openshift and project (#67)
    Merged PR-67

--- app-web/src/hoc/Layout.js
+++ app-web/src/hoc/Layout.js
@@ -12,7 +12,7 @@ import '../assets/styles/page.css';
 // layout local componenets
 import PrimaryHeader from '../components/PrimaryHeader/PrimaryHeader';
 // redux & auth
-import implicitAuthManager from '../auth';
+import { create_iam } from '../auth';
 import * as actions from '../store/actions/actions';
 
 // eslint-disable-next-line
@@ -27,6 +27,7 @@ injectGlobal`
 
 class Layout extends React.Component {
   componentDidMount() {
+    const implicitAuthManager = create_iam();
     if (this.props.useAuth) {
       implicitAuthManager.registerHooks({
         onAuthenticateSuccess: () => this.props.login(),
