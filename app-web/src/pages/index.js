3739ea8e6c795c3e2ceba2b5d722cbc45376c461
Patrick Simonian <Patrick.Simonian@gov.bc.ca>
2018-11-01 14:34:29 -0700

    update homepage

--- app-web/src/pages/index.js
+++ app-web/src/pages/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import YAML from 'js-yaml';
 import shortid from 'shortid';
+import groupArray from 'group-array';
 import { Flag } from 'flag';
 import { groupBy, flattenAllSourceDevhubGithub } from '../utils/dataMassager';
 import { GITHUB_ISSUES_ROUTE } from '../constants/routes';
 // local components
 import Layout from '../hoc/Layout';
 import Cards from '../components/Cards/Cards';
+import styles from './index.module.css';
 
 const Index = ({ data: { pathfinder, allSourceDevhubGithub } }) => {
   const yamlData = YAML.safeLoad(pathfinder.data.organization.repository.resources.yaml);
@@ -16,6 +18,7 @@ const Index = ({ data: { pathfinder, allSourceDevhubGithub } }) => {
     title: entry.description,
     description: entry.details,
     sourceName: 'Pathfinder',
+    resourcePath: entry.link,
   }));
 
   const groupedPathFinderData = groupBy(mappedEntries, 'category');
@@ -23,7 +26,7 @@ const Index = ({ data: { pathfinder, allSourceDevhubGithub } }) => {
     <Cards
       key={shortid.generate()}
       topic={gd.category}
-      sourcePath={gd.sourcePath}
+      sourcePath={gd.data[0].sourcePath}
       cards={gd.data}
     />
   ));
@@ -35,36 +38,29 @@ const Index = ({ data: { pathfinder, allSourceDevhubGithub } }) => {
     title: dhnode.childMarkdownRemark.frontmatter.title,
     description: dhnode.childMarkdownRemark.frontmatter.description,
   }));
-
   const groupedGithubData = groupBy(mappedDevhubGithubNodes, 'sourceName');
-  console.log(groupedGithubData);
   const devhubGithubResources = groupedGithubData.map(ghData => (
     <Cards
       key={shortid.generate()}
       topic={ghData.sourceName}
-      sourcePath={ghData.sourcePath}
+      sourcePath={ghData.data[0].sourcePath}
       cards={ghData.data}
     />
   ));
 
   return (
     <Layout>
-      <main role="main" className="main">
+      <main role="main" className={styles.Main}>
         <h1>Welcome</h1>
         <h2>
           <em>We are here to help</em>
         </h2>
-        <p className="para">
+        <p>
           This is the front door to the developer community of the BC Government. We’re embarking on
           a journey to help developers learn new skills, discover resources and create amazing
           applications for government.
         </p>
-        <p className="para">
-          This is our first release and we’re planning to have new material added to this site every
-          sprint (2 weeks), so please visit often to check our progress.
-        </p>
-        <p className="para">
-          {' '}
+        <p>
           If you’d like to comment, offer a suggestion or ask a question you can find us by opening
           an issue in our <a href={GITHUB_ISSUES_ROUTE}>github.com</a> repository.
         </p>
