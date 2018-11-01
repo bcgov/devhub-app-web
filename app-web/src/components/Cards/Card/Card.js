661e91bfc50a2471ce95589eeec2922bf677708e
Patrick Simonian <Patrick.Simonian@gov.bc.ca>
2018-11-01 14:46:37 -0700

    add link to resource

--- app-web/src/components/Cards/Card/Card.js
+++ app-web/src/components/Cards/Card/Card.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faExternalLinkSquareAlt } from '@fortawesome/free-solid-svg-icons';
 import styles from './Card.module.css';
 import Link from '../../Common/Link';
 
 const Card = ({ sourcePath, sourceName, title, description, resourcePath }) => (
   <article className={styles.Card}>
-    <h2>{title}</h2>
+    <h2>
+      <Link to={resourcePath}>
+        <FontAwesomeIcon icon={faExternalLinkSquareAlt} />
+        <span>{title}</span>
+      </Link>
+    </h2>
     <div className={styles.Body}>
       <p>{description}</p>
     </div>
