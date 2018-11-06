1b4c2af4bd4149afcc45978c9798114af6e21bc2
Patrick Simonian <Patrick.Simonian@gov.bc.ca>
2018-11-06 12:58:00 -0800

    update styles and scroll to functionality in nav

--- app-web/src/components/DesignSystem/Navigation.js
+++ app-web/src/components/DesignSystem/Navigation.js
@@ -1,32 +1,41 @@
-import React from 'react';
+import React, { Component } from 'react';
 import shortid from 'shortid';
 import PropTypes from 'prop-types';
 import Link from '../Common/Link';
 import styles from './Navigation.module.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBars } from '@fortawesome/free-solid-svg-icons';
-const Navigation = ({ components }) => {
-  // map over components and generate links
-  const links = components.map(({ node: { resourcePath, childMarkdownRemark } }) => (
-    <li key={shortid.generate()}>
-      <Link
-        to={resourcePath}
-        activeStyle={{
-          backgroundColor: '#fff',
-          textDecoration: 'underline',
-        }}
-      >
-        {childMarkdownRemark.frontmatter.title}
-      </Link>
-    </li>
-  ));
 
-  return (
-    <nav className={styles.Navigation}>
-      <ul className={styles.List}>{links}</ul>
-    </nav>
-  );
-};
+class Navigation extends Component {
+  componentDidMount() {
+    // scroll into view of active link if exists
+    const activeLi = document.querySelector(`.${styles.Navigation} li[data-active="true"]`);
+    activeLi.scrollIntoView();
+  }
+
+  render() {
+    const { components } = this.props;
+    // map over components and generate links
+    const links = components.map(({ node: { resourcePath, childMarkdownRemark } }) => (
+      <li key={shortid.generate()} data-active={this.props.activeLink.pathname === resourcePath}>
+        <Link
+          to={resourcePath}
+          activeStyle={{
+            backgroundColor: '#fff',
+            textDecoration: 'underline',
+          }}
+        >
+          {childMarkdownRemark.frontmatter.title}
+        </Link>
+      </li>
+    ));
+    return (
+      <nav className={styles.Navigation}>
+        <ul className={styles.List}>{links}</ul>
+      </nav>
+    );
+  }
+}
 
 export const query = graphql`
   fragment NavigationFragment on SourceDevhubGithub {
