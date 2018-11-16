1c3e8b58ca1643736894815f43ac6243b015fe10
ShellyXueHan <36056216+ShellyXueHan@users.noreply.github.com>
2018-11-16 09:51:19 -0800

    Feature: cards pagination (#96)
    * add toggle component
    
    * use toggle component to show all cards, and set cards limit as constants
    
    * add "transform-class-properties" to babel setting to get rid of unit test error of toggle component state
    
    * support mobile view
    
    * unit test
    
    * add missing proptypes
    
    * set unit test CI to true
    
    * increase bc memory limit for intermediate node js image (jest uses lots of memory)
    
    * card title no overflow
    
    * update button style to pure text

--- app-web/src/components/Cards/Cards.js
+++ app-web/src/components/Cards/Cards.js
@@ -6,7 +6,9 @@ import { faGithub } from '@fortawesome/fontawesome-free-brands';
 import styles from './Cards.module.css';
 import Card from './Card/Card';
 import Link from '../Common/Link';
+import Toggle from './Toggle';
 import { ARIA_LABEL_REPO } from '../../constants/strings';
+import { LARGE_SCREEN_LIMIT, SMALL_SCREEN_LIMIT } from '../../constants/ui';
 
 const Cards = ({ topic, sourcePath, cards }) => {
   const cardComponents = cards.map(c => <Card {...c} key={shortid.generate()} />);
@@ -22,7 +24,12 @@ const Cards = ({ topic, sourcePath, cards }) => {
         <h1>{topic}</h1>
         {octoKat}
       </div>
-      <div className={styles.Cards}>{cardComponents}</div>
+      <div className={styles.LargeView}>
+        <Toggle cardComponents={cardComponents} cardLimits={LARGE_SCREEN_LIMIT} />
+      </div>
+      <div className={styles.MobileView}>
+        <Toggle cardComponents={cardComponents} cardLimits={SMALL_SCREEN_LIMIT} />
+      </div>
     </section>
   );
 };
