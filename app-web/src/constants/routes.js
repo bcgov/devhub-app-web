3abc2ebc8ed1abd2ecb892cf1e31ae5a699dda8d
Patrick Simonian <Patrick.Simonian@gov.bc.ca>
2018-11-20 14:41:36 -0800

    Add dynamic page create to gatsby configurations (#113)
    Merged PR-113

--- app-web/src/constants/routes.js
+++ app-web/src/constants/routes.js
@@ -1,2 +1,10 @@
 export const HOME_ROUTE = '/';
 export const GITHUB_ISSUES_ROUTE = 'https://github.com/bcgov/devhub-app-web/issues';
+// used in createPages.js as well as for generating anchor tags based on the different resource
+// types
+export const FILTER_BASE_ROUTE = '/resources';
+export const DOCUMENTS_ROUTE = `${FILTER_BASE_ROUTE}/documents`;
+export const SELF_SERVICE_ROUTE = `${FILTER_BASE_ROUTE}/self-service`;
+export const COMPONENTS_ROUTE = `${FILTER_BASE_ROUTE}/components`;
+export const PROJECTS_ROUTE = `${FILTER_BASE_ROUTE}/projects`;
+export const PEOPLE_ROUTE = `${FILTER_BASE_ROUTE}/people`;
