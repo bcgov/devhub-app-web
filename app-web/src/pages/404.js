b89fc69aa12b7f99ea518752b6d5a41ce2b54282
Raj Persram <raj.persram@gov.bc.ca>
2018-11-21 15:41:54 -0800

    task-430 Designed and implemented a 404 page in React and Gatsby. First draft

--- app-web/src/pages/404.js
+++ app-web/src/pages/404.js
@@ -2,21 +2,13 @@ import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import Link from '../components/Common/Link';
 import { faSadTear } from '@fortawesome/free-solid-svg-icons';
+import VennDiagram from '../components/UI/VennDiagram/VennDiagram';
 
-const styles = {
-  fontSize: '40px',
-  textAlign: 'center',
-  padding: '10px',
-};
 
 const NotFound = ({ location }) => (
-  <div style={styles}>
-    <h1>
-      404 <FontAwesomeIcon icon={faSadTear} aria-label="location not found" />
-    </h1>
-    <p>
-      This is not the web page you are looking for. <Link to="/">Back Home</Link>.
-    </p>
+  <div>
+    <h1>404 Error - Page Not Found</h1>
+    <VennDiagram />
   </div>
 );
 
