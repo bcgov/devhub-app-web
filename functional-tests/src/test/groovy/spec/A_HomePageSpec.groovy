8c516e3040a6f68f226232acf8e33c3680de9fc6
ShellyXueHan <shellshell456@gmail.com>
2018-11-21 12:14:38 -0800

    update test cases for devhub home page

--- functional-tests/src/test/groovy/spec/A_HomePageSpec.groovy
+++ functional-tests/src/test/groovy/spec/A_HomePageSpec.groovy
@@ -6,16 +6,25 @@ import spock.lang.*
 import pages.*
 
 @Title("Devhub Homepage")
-@Narrative("I can go to home page")
+@Narrative("I can go to home page and login")
 @Stepwise
 class  A_HomePageSpec extends GebReportingSpec {
-  def "I see list of places in the home page"(){
+  def "I see the Cards Containers in the home page"(){
     when:"I go to the homepage"
     to HomePage
 
-    then: "I see Learning session"
-    LearnButton
-    DoButton
-    DeliveryButton
+    then: "I am not logged in"
+
+    and: "I click on login button"
+
+    then: "I should be redirected to the login page"
+
+    and: "I enter my credential and submit"
+    // Dotenv dotenv = Dotenv.configure().directory("./").load()
+    // IDIRusername = dotenv.get("IDIR_USERNAME")
+    // IDIRpassword = dotenv.get("IDIR_PASSWORD")
+
+    then: "I should be at homepage and logged in"
+    at HomePage
   }
 }
