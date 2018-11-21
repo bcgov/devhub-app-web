724be39f1e52b51819fa7bf3d4951891fa650cc3
Patrick Simonian <Patrick.Simonian@gov.bc.ca>
2018-11-21 11:40:49 -0800

    Resource Filtering Feature: converted resource filtering buttons to a dropdown with mobile friendly/large screen views (#115)
    Merged PR-115

--- app-web/src/constants/ui.js
+++ app-web/src/constants/ui.js
@@ -7,3 +7,51 @@ export const BANNER_ID = 'dh-banner';
 export const MAIN_NAVIGATION_BTN = 'dh-main-nav';
 export const LARGE_SCREEN_LIMIT = 4;
 export const SMALL_SCREEN_LIMIT = 1;
+
+export const RESOURCE_TYPES = {
+  COMPONENTS: {
+    DISPLAY_NAME: 'Components',
+    VALUE: 'Components',
+    ROUTE: '/resources/components',
+  },
+  DOCUMENTATION: {
+    DISPLAY_NAME: 'Documentation',
+    VALUE: 'Documentation',
+    ROUTE: '/resources/documentation',
+  },
+  SELF_SERVICE_TOOLS: {
+    DISPLAY_NAME: 'Self-Service Tools',
+    VALUE: 'Self-Service Tools',
+    ROUTE: '/resources/self-service-tools',
+  },
+};
+
+// the 'dot prop' to get the resource type from a siphon data node
+export const SIPHON_RESOURCE_TYPE_PROP = 'resource.type';
+
+export const MAIN_NAV_CONFIG = [
+  {
+    ROUTE: '/',
+    DISPLAY_NAME: 'All',
+    SIPHON_PROP: SIPHON_RESOURCE_TYPE_PROP,
+    VALUE: 'All',
+  },
+  {
+    ROUTE: RESOURCE_TYPES.COMPONENTS.ROUTE,
+    DISPLAY_NAME: RESOURCE_TYPES.COMPONENTS.DISPLAY_NAME,
+    SIPHON_PROP: SIPHON_RESOURCE_TYPE_PROP,
+    VALUE: RESOURCE_TYPES.COMPONENTS.VALUE,
+  },
+  {
+    ROUTE: RESOURCE_TYPES.DOCUMENTATION.ROUTE,
+    DISPLAY_NAME: RESOURCE_TYPES.DOCUMENTATION.DISPLAY_NAME,
+    SIPHON_PROP: SIPHON_RESOURCE_TYPE_PROP,
+    VALUE: RESOURCE_TYPES.DOCUMENTATION.VALUE,
+  },
+  {
+    ROUTE: RESOURCE_TYPES.SELF_SERVICE_TOOLS.ROUTE,
+    DISPLAY_NAME: RESOURCE_TYPES.SELF_SERVICE_TOOLS.DISPLAY_NAME,
+    SIPHON_PROP: SIPHON_RESOURCE_TYPE_PROP,
+    VALUE: RESOURCE_TYPES.SELF_SERVICE_TOOLS.VALUE,
+  },
+];
