dba7a8caaef4ebff76b83abfbc970719b9f64121
Patrick Simonian <Patrick.Simonian@gov.bc.ca>
2018-11-20 16:26:12 -0800

    complete navigation

--- app-web/src/components/Navigation/PrimaryNavigation.js
+++ app-web/src/components/Navigation/PrimaryNavigation.js
@@ -19,44 +19,110 @@ import React from 'react';
 import styles from './PrimaryNavigation.module.css';
 import { connect } from 'react-redux';
 import Select from 'react-select';
+import Aux from '../../hoc/auxillary';
 import * as actions from '../../store/actions/actions';
 import { MAIN_NAV_CONFIG } from '../../constants/ui';
+import { ARIA_LABEL_FILTER_SELECT } from '../../constants/strings';
+import Hamburger from '../Common/Hamburger';
 
-export const PrimaryNavigation = ({ filterSiphonNodes }) => {
-  const navigationItems = MAIN_NAV_CONFIG.map(navConfig => ({
-    value: {
-      filterBy: navConfig.SIPHON_PROP,
-      value: navConfig.VALUE,
-    },
-    label: navConfig.DISPLAY_NAME,
-  }));
-
-  const customStyles = {
-    option: (provided, state) => ({
-      ...provided,
-      borderBottom: '1px solid #ccc',
-      color: state.isSelected ? '#036' : '#444',
-      backgroundColor: state.isFocused ? '#ccc' : '#fafafa',
-    }),
-    singleValue: (provided, state) => {
-      const opacity = state.isDisabled ? 0.5 : 1;
-      const transition = 'opacity 300ms';
-
-      return { ...provided, opacity, transition };
-    },
+export class PrimaryNavigation extends React.Component {
+  state = {
+    selectedOption: null,
+    toggled: false,
   };
 
-  return (
-    <Select
-      className={styles.PrimaryNavigation}
-      styles={customStyles}
-      options={navigationItems}
-      placeholder="Filter"
-      onChange={selectedOption =>
-        filterSiphonNodes(selectedOption.value.filterBy, selectedOption.value.value)}
-    />
-  );
-};
+  render() {
+    const { filterSiphonNodes } = this.props;
+    // convert into open objects the Select component understands
+    // value is stringified because it is used in two select components
+    // this is to prevent any odd pass by reference bugs from going on that
+    // are beyond our control
+    const navigationItems = MAIN_NAV_CONFIG.map(navConfig => ({
+      value: JSON.stringify({
+        filterBy: navConfig.SIPHON_PROP,
+        value: navConfig.VALUE,
+      }),
+      label: navConfig.DISPLAY_NAME,
+    }));
+
+    const customStylesLarge = {
+      option: (provided, state) => ({
+        ...provided,
+        borderBottom: '1px solid #ccc',
+        color: state.isSelected ? '#036' : '#444',
+        backgroundColor: state.isFocused ? '#ccc' : '#fafafa',
+      }),
+      singleValue: (provided, state) => {
+        const opacity = state.isDisabled ? 0.5 : 1;
+        const transition = 'opacity 300ms';
+        return { ...provided, opacity, transition };
+      },
+      control: (provided, state) => ({
+        ...provided,
+        width: 150,
+      }),
+    };
+    // mobile select styles
+    // hiding the dropdown menu and instead using a hamburger to activate it via state
+    const customStylesSmall = {
+      ...customStylesLarge,
+      option: (provided, state) => ({
+        ...provided,
+        borderBottom: '1px solid #ccc',
+        color: state.isSelected ? '#036' : '#444',
+        backgroundColor: state.isFocused ? '#fafafa' : '#fafafa',
+      }),
+      control: (provided, state) => ({
+        display: 'none',
+      }),
+      menu: (provided, state) => {
+        const width = 150;
+        return {
+          ...provided,
+          width,
+          transform: `translate(-${width}px, 18px)`,
+        };
+      },
+    };
+
+    return (
+      <Aux>
+        <Select
+          value={this.state.selectedOption}
+          ariaLabel={ARIA_LABEL_FILTER_SELECT}
+          className={[styles.PrimaryNavigation, styles.largeOnly].join(' ')}
+          styles={customStylesLarge}
+          options={navigationItems}
+          placeholder="Filter"
+          onChange={selectedOption => {
+            this.setState({ selectedOption: selectedOption });
+            const option = JSON.parse(selectedOption.value);
+            filterSiphonNodes(option.filterBy, option.value);
+          }}
+        />
+        {/* mobiler select */}
+        <Hamburger
+          className={styles.mobileOnly}
+          clicked={() => this.setState({ toggled: !this.state.toggled })}
+        />
+        <Select
+          value={this.state.selectedOption}
+          ariaLabel={ARIA_LABEL_FILTER_SELECT}
+          className={[styles.PrimaryNavigation, styles.mobileOnly].join(' ')}
+          styles={customStylesSmall}
+          options={navigationItems}
+          placeholder=""
+          menuIsOpen={this.state.toggled}
+          onChange={selectedOption => {
+            this.setState({ selectedOption: selectedOption, toggled: false });
+            const option = JSON.parse(selectedOption.value);
+            filterSiphonNodes(option.value.filterBy, option.value.value);
+          }}
+        />
+      </Aux>
+    );
+  }
+}
 
 const mapDispatchToProps = dispatch => {
   return {
