8e64feb1d90238a7ba24d7ce0cca70933e741ade
Patrick Simonian <Patrick.Simonian@gov.bc.ca>
2018-11-23 11:03:29 -0800

    update unit tests

--- app-web/plugins/gatsby-remark-path-transform/__tests__/utils.test.js
+++ app-web/plugins/gatsby-remark-path-transform/__tests__/utils.test.js
@@ -59,8 +59,6 @@ describe('gatsby-remark-path-transform', () => {
       transformRelativePaths({ getNode, markdownAST, markdownNode }, { converter });
 
       expect(converter).toHaveBeenCalledWith('image', oldURL, GRAPH_QL_PARENT_NODE);
-      expect(converter).toHaveBeenLastCalledWith('link', markdownAST.url, GRAPH_QL_PARENT_NODE);
-      expect(converter).toHaveBeenCalledTimes(2);
       // expect url to hvae been changed
       expect(oldURL).not.toBe(markdownAST.url);
     });
