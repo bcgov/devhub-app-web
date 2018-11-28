cec96fe6f5e26fa80e8af5a0cfd5799ece5c0ab8
Patrick Simonian <Patrick.Simonian@gov.bc.ca>
2018-11-28 14:59:32 -0800

    integrate images

--- app-web/src/components/Cards/Card/DocumentCard.js
+++ app-web/src/components/Cards/Card/DocumentCard.js
@@ -3,11 +3,13 @@ import PropTypes from 'prop-types';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faExternalLinkSquareAlt } from '@fortawesome/free-solid-svg-icons';
 import DotDotDot from 'react-dotdotdot';
+import Image from 'react-image';
 import styles from './Card.module.css';
 import Card from '../../../hoc/Card';
 import Link from '../../UI/Link/Link';
 import { ARIA_LABEL_RESOURCE, ARIA_LABEL_REPO } from '../../../constants/ariaLabels';
-const DocumentCard = ({ sourcePath, sourceName, title, description, resourcePath }) => (
+
+const DocumentCard = ({ sourcePath, sourceName, title, description, resourcePath, image }) => (
   <Card>
     <h2 title={title}>
       <Link to={resourcePath} aria-label={ARIA_LABEL_RESOURCE}>
@@ -19,6 +21,7 @@ const DocumentCard = ({ sourcePath, sourceName, title, description, resourcePath
     </h2>
     <div className={styles.Body}>
       <p>{description}</p>
+      <Image src={image} />
     </div>
     <div className={styles.Actions}>
       {/* <ul>
@@ -38,6 +41,11 @@ DocumentCard.propTypes = {
   title: PropTypes.string.isRequired,
   description: PropTypes.string.isRequired,
   resourcePath: PropTypes.string.isRequired,
+  image: PropTypes.string,
+};
+
+DocumentCard.defaultProps = {
+  image: '',
 };
 
 export default DocumentCard;
