dbfc1ba8304b44251fe985a0643251fbddf3b3c5
Patrick Simonian <Patrick.Simonian@gov.bc.ca>
2018-12-05 15:53:24 -0800

    abstract away primary filter from layout so that we don't need to logic to render it based on page path

--- app-web/src/pages/index.js
+++ app-web/src/pages/index.js
@@ -8,6 +8,7 @@ import { groupBy } from '../utils/dataMassager';
 import { GITHUB_ISSUES_ROUTE } from '../constants/routes';
 // local components
 import Layout from '../hoc/Layout';
+import PrimaryFilter from '../components/PrimaryFilter/PrimaryFilter';
 import Cards from '../components/Cards/Cards';
 import styles from './index.module.css';
 import CardFilterButton from '../components/CardFilterButton/CardFilterButton';
@@ -20,7 +21,7 @@ export class Index extends Component {
   }
 
   render() {
-    const { nodes, location: { pathname } } = this.props;
+    const { nodes, location: { pathname }, menuToggled, toggleMenu } = this.props;
     let mappedSiphonNodes = [];
     if (nodes && nodes.length) {
       mappedSiphonNodes = nodes
@@ -50,52 +51,54 @@ export class Index extends Component {
     ));
 
     return (
-      <Layout path={pathname}>
-        <main role="main" className={styles.Main}>
+      <Layout showHamburger hamburgerClicked={toggleMenu}>
+        <Flag name="features.sourceFiltering">
+          <PrimaryFilter />
+          {menuToggled ? <PrimaryFilter mobile /> : null}
+        </Flag>
+
+        <main role="main" className={[styles.Main, 'container'].join(' ')}>
           <section className="jumbotron text-center">
-            <div className="container">
-              <h1 className="jumbotron-heading">Welcome.</h1>
+            <h1 className="jumbotron-heading">Welcome.</h1>
 
-              <h3> We are here to help.</h3>
+            <h3> We are here to help.</h3>
 
-              <p className="lead">
-                This is the front door to the developer community of the BC Government. The aim of
-                the DevHub is to help developers and digital product teams learn new skills and
-                discover resources to use on their journeys of creating amazing applications for
-                government.
-              </p>
-              <p>
-                In the future, we plan to offer a variety of cool and useful ways to organize and
-                navigate DevHub resources. For now, you can tell us who you are below and we'll
-                tailor the set of resources shown just for you.
-              </p>
-              <div className={'d-flex justify-content-center align-items-center'}>
-                <CardFilterButton
-                  filterByProperty={'attributes.persona'}
-                  filterByValue={'Developer'}
-                  className={['btn btn-outline-primary', styles.PersonaButton].join(' ')}
-                >
-                  I'm a Developer
-                </CardFilterButton>
-                <CardFilterButton
-                  filterByProperty={'attributes.persona'}
-                  filterByValue={'Designer'}
-                  className={['btn btn-outline-success', styles.PersonaButton].join(' ')}
-                >
-                  I'm a Designer
-                </CardFilterButton>
-              </div>
-
-              <blockquote className="blockquote">
-                <p className="mb-0">Thanks for visiting!</p>
-                <footer className="blockquote-footer">The DevHub Team.</footer>
-              </blockquote>
-
-              <p className="text-muted">
-                PS. If you’d like to comment, offer a suggestion or ask a question you can find us
-                by opening an issue in our <a href={GITHUB_ISSUES_ROUTE}>GitHub</a> repository.
-              </p>
+            <p className="lead">
+              This is the front door to the developer community of the BC Government. The aim of the
+              DevHub is to help developers and digital product teams learn new skills and discover
+              resources to use on their journeys of creating amazing applications for government.
+            </p>
+            <p>
+              In the future, we plan to offer a variety of cool and useful ways to organize and
+              navigate DevHub resources. For now, you can tell us who you are below and we'll tailor
+              the set of resources shown just for you.
+            </p>
+            <div className={'d-flex justify-content-center align-items-center'}>
+              <CardFilterButton
+                filterByProperty={'attributes.persona'}
+                filterByValue={'Developer'}
+                className={['btn btn-outline-primary', styles.PersonaButton].join(' ')}
+              >
+                I'm a Developer
+              </CardFilterButton>
+              <CardFilterButton
+                filterByProperty={'attributes.persona'}
+                filterByValue={'Designer'}
+                className={['btn btn-outline-success', styles.PersonaButton].join(' ')}
+              >
+                I'm a Designer
+              </CardFilterButton>
             </div>
+
+            <blockquote className="blockquote">
+              <p className="mb-0">Thanks for visiting!</p>
+              <footer className="blockquote-footer">The DevHub Team.</footer>
+            </blockquote>
+
+            <p className="text-muted">
+              PS. If you’d like to comment, offer a suggestion or ask a question you can find us by
+              opening an issue in our <a href={GITHUB_ISSUES_ROUTE}>GitHub</a> repository.
+            </p>
           </section>
           <div className={styles.CardContainer}>
             <Flag name="features.githubResourceCards">{SiphonResources}</Flag>
@@ -163,12 +166,14 @@ export const resourceQuery = graphql`
 const mapStateToProps = state => {
   return {
     nodes: state.siphon.filteredNodes,
+    menuToggled: state.ui.mainNavigationToggled,
   };
 };
 
 const mapDispatchToProps = dispatch => {
   return {
     loadSiphonNodes: nodes => dispatch(actions.loadSiphonNodes(nodes)),
+    toggleMenu: () => dispatch(actions.toggleMainNavigation()),
   };
 };
 
