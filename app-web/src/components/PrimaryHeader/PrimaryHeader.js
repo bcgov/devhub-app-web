dc12de6c2b305a26e0d7b9136bdbfb05fe921017
Patrick Simonian <Patrick.Simonian@gov.bc.ca>
2018-12-07 09:49:21 -0800

    remove unused bits from primary header

--- app-web/src/components/PrimaryHeader/PrimaryHeader.js
+++ app-web/src/components/PrimaryHeader/PrimaryHeader.js
@@ -2,14 +2,13 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Flag } from 'flag';
 import classes from './PrimaryHeader.module.css';
-import { APP_TITLE } from '../../constants/ariaLabels';
 import Banner from '../UI/Banner/Banner';
 import Hamburger from '../UI/Hamburger/Hamburger';
 import Login from '../Auth/Login/Login';
 
 export const PrimaryHeader = ({ showHamburger, hamburgerClicked }) => (
   <header className={classes.PrimaryHeader}>
-    <Banner title={APP_TITLE} navigateOnClickPath="/" />
+    <Banner />
     <div className={classes.Other}>
       <Flag name="features.login">
         <Login />
