652bcb1e0925339848186febabb49ab1a20f5136
Patrick Simonian <Patrick.Simonian@gov.bc.ca>
2019-01-03 12:55:08 -0800

    refactor header out of github template

--- app-web/src/hoc/GithubTemplateLayout.js
+++ app-web/src/hoc/GithubTemplateLayout.js
@@ -4,45 +4,33 @@ import PropTypes from 'prop-types';
 import styles from './TemplateLayout.module.css';
 // layout local componenets
 import SourceNavigation from '../components/SourceNavigation/SourceNavigation';
-import Layout from './Layout';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faGithub } from '@fortawesome/fontawesome-free-brands';
-import { faCodeBranch } from '@fortawesome/free-solid-svg-icons';
+import PrimaryHeader from '../components/PrimaryHeader/PrimaryHeader';
+import PrimaryFooter from '../components/PrimaryFooter/PrimaryFooter';
+
+import Header from '../components/GithubTemplate/Header/Header';
 
 const GithubTemplateLayout = ({ siphonData, nav, pathname, children }) => {
   // SourceNavigation doesn't show up if there are no links
   const sourceNavigation =
     nav.edges.length > 1 ? <SourceNavigation components={nav.edges} activeLink={pathname} /> : null;
   return (
-    <Layout>
+    <div className="layout">
+      <PrimaryHeader />
       <div className={styles.TemplateContainer}>
         <div className={styles.SidePanel}>
-          <header className={styles.Header}>
-            <h1>{siphonData.source.displayName}</h1>
-            <ul className={styles.SourceTags}>
-              <li>
-                <a href={siphonData.resource.originalSource}>
-                  <FontAwesomeIcon
-                    icon={faGithub}
-                    aria-label={`View the original source ${siphonData.fileName} on github`}
-                  />
-                </a>
-              </li>
-              <li>
-                <a href={`${siphonData.source.sourcePath}fork`}>
-                  <FontAwesomeIcon
-                    icon={faCodeBranch}
-                    aria-label={`Fork ${siphonData.source.name} on github`}
-                  />
-                </a>
-              </li>
-            </ul>
-          </header>
+          <Header
+            title={siphonData.source.displayName}
+            originalSource={siphonData.resource.originalSource}
+            fileName={siphonData.fileName}
+            sourcePath={siphonData.source.sourcePath}
+            repo={siphonData.source.name}
+          />
           {sourceNavigation}
         </div>
         <main className={styles.Content}>{children}</main>
       </div>
-    </Layout>
+      <PrimaryFooter />
+    </div>
   );
 };
 
