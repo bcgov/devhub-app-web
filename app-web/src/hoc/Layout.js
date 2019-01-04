e9453ab434010308907cf13b74fb331a385ef7d0
Patrick Simonian <Patrick.Simonian@gov.bc.ca>
2019-01-04 14:39:05 -0800

    revert filter components back into index page to make layout more usable

--- app-web/src/hoc/Layout.js
+++ app-web/src/hoc/Layout.js
@@ -1,15 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { Flag } from 'flag';
-import FLAGS from '../constants/featureflags';
 // stylesheets
 import '../assets/styles/index.css';
 // layout local componenets
 import PrimaryHeader from '../components/PrimaryHeader/PrimaryHeader';
 import PrimaryFooter from '../components/PrimaryFooter/PrimaryFooter';
-import PrimaryFilter from '../components/PrimaryFilter/PrimaryFilter';
-import Dropmenu from '../components/Dropmenu/Dropmenu';
 
 // redux & auth
 import { create_iam } from '../auth';
@@ -36,11 +32,6 @@ export class Layout extends React.Component {
     return (
       <div className="layout">
         <PrimaryHeader showHamburger hamburgerClicked={toggleMenu} />
-        <Flag name={`features.${FLAGS.SOURCE_FILTERING}`}>
-          <PrimaryFilter />
-        </Flag>
-        {/* hamburger icon controlled menu */}
-        <Dropmenu menuToggled />
 
         {children}
 
