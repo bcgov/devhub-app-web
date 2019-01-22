1f81722a24b48bbb043be6551fada5a21599ae9d
Patrick Simonian <Patrick.Simonian@gov.bc.ca>
2019-01-22 14:59:43 -0800

    set routes config

--- app-web/src/constants/routes.js
+++ app-web/src/constants/routes.js
@@ -2,10 +2,9 @@ export const HOME_ROUTE = '/';
 export const GITHUB_ISSUES_ROUTE = 'https://github.com/bcgov/devhub-app-web/issues';
 // used in createPages.js as well as for generating anchor tags based on the different resource
 // types
-export const FILTER_BASE_ROUTE = '/resources';
-export const DOCUMENTS_ROUTE = `${FILTER_BASE_ROUTE}/documents`;
-export const SELF_SERVICE_ROUTE = `${FILTER_BASE_ROUTE}/self-service`;
-export const COMPONENTS_ROUTE = `${FILTER_BASE_ROUTE}/components`;
-export const PROJECTS_ROUTE = `${FILTER_BASE_ROUTE}/projects`;
-export const PEOPLE_ROUTE = `${FILTER_BASE_ROUTE}/people`;
-export const REPOS_ROUTE = `${FILTER_BASE_ROUTE}/repositories`;
+export const DOCUMENTS_ROUTE = `/documentation`;
+export const SELF_SERVICE_ROUTE = `/self-service-tools`;
+export const COMPONENTS_ROUTE = `/components`;
+export const PROJECTS_ROUTE = `/projects`;
+export const PEOPLE_ROUTE = `/people`;
+export const REPOS_ROUTE = `/repositories`;
