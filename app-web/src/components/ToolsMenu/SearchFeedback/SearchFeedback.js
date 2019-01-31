0babaf2bf90e6a1dffec1f5635c563f306aef075
Patrick Simonian <Patrick.Simonian@gov.bc.ca>
2019-01-30 16:45:24 -0800

    fix up search feed back links to redirect correctly

--- app-web/src/components/ToolsMenu/SearchFeedback/SearchFeedback.js
+++ app-web/src/components/ToolsMenu/SearchFeedback/SearchFeedback.js
@@ -20,8 +20,8 @@ import PropTypes from 'prop-types';
 import { Link } from 'gatsby';
 
 const SearchFeedback = ({ searchCount, totalNodeCount, searchWordLength }) => {
+  const path = `/?q= &search=true`;
   let caption = null;
-  const path = `/?q=`;
   if (searchCount === 0) {
     caption = (
       <p>
