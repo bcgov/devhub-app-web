d3a5d19cccbea4740fe65f4326a97045c77c8b10
Davis Levine <davis.levine@gov.bc.ca>
2019-02-01 16:43:48 -0800

    Header changes

--- app-web/src/components/UI/GovLogo/GovLogo.js
+++ app-web/src/components/UI/GovLogo/GovLogo.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import Aux from '../../../hoc/auxillary';
-import logo from '../../../assets/images/logo.png';
+import logo from '../../../assets/images/BCID_H_rgb_rev.svg';
 import logoMobile from '../../../assets/images/logo.svg';
 import styles from './GovLogo.module.css';
 
 const GovLogo = () => (
   <Aux>
-    <img src={logo} className="large-screen" alt="Government of British Columbia" />
+  <img
+  src={logo}
+  className={[styles.DesktopLogo, 'large-screen'].join(' ')}
+  alt="Government of British Columbia"
+  />
+
     <img
       src={logoMobile}
       className={[styles.MobileLogo, 'small-screen'].join(' ')}
