12b09f2a5975dbb589f35497545cc9b8725687ec
Patrick Simonian <Patrick.Simonian@gov.bc.ca>
2019-02-12 15:25:14 -0800

    remove old tests and add tests for card spec

--- app-web/__tests__/components/Card.test.js
+++ app-web/__tests__/components/Card.test.js
@@ -1,12 +1,39 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import DefaultCard from '../../src/components/Cards/Card/DefaultCard';
+import Card from '../../src/components/Cards/Card/Card';
+import validURL from 'valid-url';
+jest.mock('valid-url', () => ({
+  isWebUri: jest.fn(() => true),
+}));
 
 describe('Card Component', () => {
   it('matches snapshot', () => {
     const wrapper = shallow(
-      <DefaultCard title="title" description="description" resourcePath="resourcePath" />,
+      <Card title="title" description="description" resourceType="resourceType" link="fofofo" />,
     );
     expect(wrapper).toMatchSnapshot();
+  });
+
+  it("doesn't render the resource type when not provided", () => {
+    validURL.isWebUri.mockImplementation(() => true);
+    const wrapper = shallow(
+      <Card title="title" description="description" link="https://google.com" />,
+    );
+
+    expect(wrapper.find('.ResourceType').length).toBe(0);
+  });
+
+  it('renders an external link icon besides the resource type when link is a web uri', () => {
+    validURL.isWebUri.mockImplementation(() => true);
+    const wrapper = shallow(
+      <Card
+        title="title"
+        description="description"
+        resourceType="resourceType"
+        link="https://google.com"
+      />,
+    );
+
+    expect(wrapper.find('.ResourceType small').length).toBe(1);
   });
 });
