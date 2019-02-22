cd724550ca7a89ad85f919141fd4bdecf8d31831
Shea Phillips <shea.phillips@gmail.com>
2019-02-22 13:50:41 -0800

    - Adding typographyjs integration (plugins) - add initial configuration (typography.js file) to allow typography settings within the Devhub to be managed via code. - add google fonts prefetch plugin (for Noto Sans) so fonts are downloaded at build time and available statically from the devhub caddy server at runtime (vs. pulled from GitHub)

---
+++ app-web/typography.js
@@ -0,0 +1,11 @@
+import Typography from 'typography'
+
+const typography = new Typography({
+    baseFontSize: '18px',
+    baseLineHeight: 1.6,
+    headerFontFamily: ['Noto Sans', 'Verdana', 'Arial', 'sans-serif'],
+    bodyFontFamily: ['Noto Sans', 'Verdana', 'Arial', 'sans-serif'],
+    scaleRatio: 1.2
+});
+
+export default typography
