f88acadb02be2922563cc696314f56a015d07754
Patrick Simonian <Patrick.Simonian@gov.bc.ca>
2019-02-26 09:05:51 -0800

    add spec for side drawer

--- app-web/__tests__/components/Sidedrawer.test.js
+++ app-web/__tests__/components/Sidedrawer.test.js
@@ -3,9 +3,11 @@ import { shallow } from 'enzyme';
 import SideDrawer from '../../src/components/SideDrawer/SideDrawer';
 
 describe('404 Page', () => {
-  let wrapper;
+  let wrapper, closeDrawer;
+  let title = 'Foo';
   beforeEach(() => {
-    wrapper = shallow(<SideDrawer title="this is a title" closeDrawer={jest.fn()} />);
+    closeDrawer = jest.fn();
+    wrapper = shallow(<SideDrawer title={title} closeDrawer={closeDrawer} />);
   });
 
   test('it matches snapshot', () => {
@@ -13,6 +15,22 @@ describe('404 Page', () => {
   });
 
   test('it calls drawer close fn when x button is clicked', () => {
-    const button = wrapper.find;
+    const button = wrapper.find('IconButton');
+    button.simulate('click');
+
+    expect(closeDrawer).toHaveBeenCalled();
+  });
+
+  test('it displays the title when passed in', () => {
+    const h2 = wrapper.find('H2');
+    expect(h2.containsMatchingElement(<span>{title}</span>)).toBeTruthy();
+  });
+
+  test('it allow children to be passed', () => {
+    const child = <p>yoyoyoy</p>;
+    expect(wrapper.containsMatchingElement(child)).not.toBeTruthy();
+
+    wrapper.setProps({ children: child }).update();
+    expect(wrapper.containsMatchingElement(child)).toBeTruthy();
   });
 });
