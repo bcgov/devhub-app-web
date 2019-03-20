68993fa9450759259071f36afca5f51bc6265648
Patrick Simonian <Patrick.Simonian@gov.bc.ca>
2019-03-20 13:53:41 -0700

    change styles to emotion and rename to index

--- app-web/src/components/Search/index.js
+++ app-web/src/components/Search/index.js
@@ -16,12 +16,28 @@ limitations under the License.
 Created by Patrick Simonian
 */
 import React, { Component } from 'react';
+import styled from '@emotion/styled';
 import { SEARCH } from '../../messages';
 import { ARIA_LABEL_SEARCH_BUTTON, ARIA_LABEL_SEARCH_INPUT } from '../../constants/ariaLabels';
 import PropTypes from 'prop-types';
 import Button from '../UI/Button/Button';
 import Input from '../UI/Input/Input';
-import styles from './Search.module.css';
+
+const Container = styled.div`
+  display: flex;
+  align-items: center;
+  > input {
+    margin: 0 4px;
+    padding: 4px 5px;
+    flex-basis: 250px;
+    flex-grow: 1;
+    transition: all 0.25s;
+  }
+  > button {
+    padding: 4px 15px;
+    margin: 4px;
+  }
+`;
 
 export class Search extends Component {
   state = {
@@ -52,9 +68,10 @@ export class Search extends Component {
   };
 
   render() {
-    const { inputConfig, css } = this.props;
+    const { inputConfig, ...rest } = this.props;
+    // rest param is mostly used to pass custom styling to the container if necessaary
     return (
-      <div className={styles.Search} css={css}>
+      <Container {...rest}>
         <Input
           type="text"
           aria-label={ARIA_LABEL_SEARCH_INPUT}
@@ -67,7 +84,7 @@ export class Search extends Component {
         <Button type="primary" aria-label={ARIA_LABEL_SEARCH_BUTTON} clicked={() => this.search()}>
           {SEARCH.button.defaultMessage}
         </Button>
-      </div>
+      </Container>
     );
   }
 }
