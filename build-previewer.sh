eb3671ed648109f2797bf346fb1e550cd459cd2b
Shea Phillips <shea.phillips@gmail.com>
2019-04-26 20:54:22 -0700

    changes for previewer flow

--- build-previewer.sh
+++ build-previewer.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-docker-compose -f docker-compose.preview.yml build --no-cache
+docker-compose -f docker-compose.preview.yml build --build-arg GIT_REF=$(git rev-parse --short HEAD) --no-cache
 #todo push to docker hub
