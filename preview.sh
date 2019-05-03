11e1e4c8b13dfb1a4125abb1dd3b5dccd158e943
Shea Phillips <shea.phillips@gmail.com>
2019-05-02 17:01:04 -0700

    Tweaks to preview-related scripts to handle situations where the latest published image is not what we want locally.

--- preview.sh
+++ preview.sh
@@ -1,6 +1,15 @@
 #!/usr/bin/env bash
 
-docker pull bcgovimages/devhub-previewer
+read -n1 -r -p "Press 'P' to retrieve image from DockerHub. Any other key to continue with local image." key
+
+if [ "$key" = 'P' ]; then
+    echo "Pulling image from DockerHub..."
+    docker pull bcgovimages/devhub-previewer
+else
+    echo "Using local image."
+fi
+
+
 docker-compose down
 docker-compose up
 docker-compose down
