835a0a832a9d422312a5064640504df56c1c3dd7
Derek Siemens <Derek.Siemens@gov.bc.ca>
2019-06-04 14:36:43 -0700

    proof of concept

--- app-web/src/templates/events.js
+++ app-web/src/templates/events.js
@@ -10,10 +10,21 @@ import { Event } from '../components/Event';
 import { flattenGatsbyGraphQL } from '../utils/dataHelpers';
 import { EVENTS } from '../constants/ui';
 import { EMOTION_BOOTSTRAP_BREAKPOINTS } from '../constants/designTokens';
+import CardCarousel from '../components/CardCarousel/CardCarousel';
+//import cloneDeep from 'lodash/cloneDeep';
+import { Container, LinkContainer } from '../components/home/index';
+import { ChevronLink } from '../components/UI/Link';
+//import ResourcePreview from '../components/home/ResourcePreview';
+//import CollectionsContainer from '../components/Home/CollectionsContainer';
+//import merge from 'lodash/merge';
 
 export const TEST_IDS = {
   alert: 'events-container',
 };
+const ContainerBoarder = styled(Container)`
+  max-width: 862px;
+  margin: 20px 0;
+`;
 
 const Blockqoute = styled.blockquote`
   padding: 10px;
@@ -37,10 +48,64 @@ const CardContainer = styled.div`
   }
 `;
 
-export const EventsPage = ({ data: { allEventbriteEvents } }) => {
+export const EventsPage = ({ data: { allEventbriteEvents, allDevhubSiphon } }) => {
   const events = flattenGatsbyGraphQL(allEventbriteEvents.edges);
   // filter out any events that are passed today
   const currentEvents = events.filter(e => e.start.daysFromNow <= 0);
+
+  //Get all the cards on the site
+  const cards = flattenGatsbyGraphQL(allDevhubSiphon.edges);
+  //filter to just get the cards for the Community Enablers and Events topic
+  const communityCards = cards.filter(
+    e => e.source.displayName === 'Community and Events' || e.name === 'Community and Events',
+  );
+
+  //Take the eventbrite events and format their data to be able to be used as a card
+  //let currentCards = cloneDeep(currentEvents);
+  /*currentCards = currentCards.filter(e => delete e.venue).filter(e => delete e.organization);
+  currentCards.forEach(element => {
+    element.unfurl = {
+      title: element.name.text,
+      image: element.logo.original.url,
+      description:
+        element.start.month +
+        ' ' +
+        element.start.day +
+        ': ' +
+        element.description.html.replace(/<[^>]+>/g, ''), //this regular expression removes any sort of HTML related tag
+    };
+    element.resource = { type: 'Self-Service Tools', path: element.url };
+  });*/
+
+  //make this into a function in UTILS
+  const currentCards = currentEvents.map(element => {
+    const EventCards = {
+      unfurl: {
+        title: element.name.text,
+        image: element.logo.original.url,
+        description:
+          element.start.month +
+          ' ' +
+          element.start.day +
+          ': ' +
+          element.description.html.replace(/<[^>]+>/g, ''), //this regular expression removes any sort of HTML related tag
+      },
+      resource: {
+        type: 'Events',
+        path: element.url,
+      },
+    };
+    return EventCards;
+  });
+
+  //add Community and Events cards with the event cards so that they can be used in the same carousel
+  //let i = 0;
+  const finalCards = communityCards.concat(currentCards);
+  /*currentCards.forEach(element => {
+    communityCards.push(currentCards[i]);
+    i++;
+  });*/
+
   // previous events are sorted in descending order
   const previousEvents = events
     .filter(e => e.start.daysFromNow > 0)
@@ -59,6 +124,14 @@ export const EventsPage = ({ data: { allEventbriteEvents } }) => {
             Github account).
           </p>
         </Blockqoute>
+        <ContainerBoarder>
+          <CardCarousel resources={finalCards} />
+          <LinkContainer>
+            <ChevronLink to={'/Community-Enablers-and-Events/BC-Gov-Development-Community-Events'}>
+              View
+            </ChevronLink>
+          </LinkContainer>
+        </ContainerBoarder>
         {currentEvents.length > 0 ? (
           <Aux>
             <CardContainer>
@@ -130,6 +203,41 @@ export const EventData = graphql`
           }
           venue {
             name
+          }
+        }
+      }
+    }
+    allDevhubSiphon {
+      edges {
+        node {
+          id
+          name
+          owner
+          parent {
+            id
+          }
+          _metadata {
+            position
+          }
+          attributes {
+            personas
+          }
+          source {
+            displayName
+            sourcePath
+            type
+            name
+          }
+          resource {
+            path
+            type
+          }
+          unfurl {
+            title
+            description
+            type
+            image
+            author
           }
         }
       }
