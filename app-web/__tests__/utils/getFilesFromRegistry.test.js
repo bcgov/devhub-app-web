3d2bc303d7d86104b2acfe7ed8a64ace98e2f486
Patrick Simonian <Patrick.Simonian@gov.bc.ca>
2019-06-14 15:55:27 -0700

    integrate gatsby github raw plugin

---
+++ app-web/__tests__/utils/getFilesFromRegistry.test.js
@@ -0,0 +1,67 @@
+import { getFilesFromRegistry } from '../../gatsby/utils/githubRaw';
+
+describe('getFilesFromRegistry', () => {
+  const registry = [
+    {
+      internal: {
+        type: 'RegistryJson',
+      },
+      name: 'Registry Item 1',
+      description: 'An agile process for teams to deliver digital services',
+      sourceProperties: {
+        sources: [
+          {
+            sourceType: 'github',
+            sourceProperties: {
+              url: 'https://github.com/bcgov/Agile-Delivery-Process',
+              owner: 'bcgov',
+              repo: 'Agile-Delivery-Process',
+              files: ['README.md', 'foo.md'],
+            },
+          },
+        ],
+      },
+    },
+    {
+      internal: {
+        type: 'RegistryJson',
+      },
+      name: 'Registry Item 2',
+      description: 'An agile process for teams to deliver digital services',
+      sourceProperties: {
+        sources: [
+          {
+            sourceType: 'github',
+            sourceProperties: {
+              url: 'https://github.com/bcgov/Agile-Delivery-Process',
+              owner: 'bar',
+              repo: 'foo',
+              branch: 'baz',
+              file: 'README.md',
+            },
+          },
+        ],
+      },
+    },
+  ];
+
+  it('returns a list of files', () => {
+    const getNode = jest.fn();
+    getNode.mockReturnValue(registry);
+    const expected = [
+      {
+        url: 'https://github.com/bcgov/Agile-Delivery-Process/blob/master/README.md',
+        topics: ['Registry Item 1'],
+      },
+      {
+        url: 'https://github.com/bcgov/Agile-Delivery-Process/blob/master/foo.md',
+        topics: ['Registry Item 1'],
+      },
+      {
+        url: 'https://github.com/bar/foo/blob/baz/README.md',
+        topics: ['Registry Item 2'],
+      },
+    ];
+    expect(getFilesFromRegistry(getNode)).toEqual(expected);
+  });
+});
