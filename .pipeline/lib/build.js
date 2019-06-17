647f89a7db7d7bdb71b81c375b9e2e278e819c5b
Shea Phillips <shea.phillips@gmail.com>
2019-06-17 11:55:15 -0700

    Adding missing config values for matomo to devhub app build script.

--- .pipeline/lib/build.js
+++ .pipeline/lib/build.js
@@ -1,14 +1,14 @@
 'use strict';
-const {OpenShiftClientX} = require('pipeline-cli')
+const {OpenShiftClientX} = require('pipeline-cli');
 const path = require('path');
 
 module.exports = (settings)=>{
-  const phases = settings.phases
-  const options = settings.options
+  const phases = settings.phases;
+  const options = settings.options;
   const oc=new OpenShiftClientX(Object.assign({'namespace':phases.build.namespace}, options));
-  const phase='build'
-  let objects = []
-  const templatesLocalBaseUrl =oc.toFileUrl(path.resolve(__dirname, '../../openshift/templates'))
+  const phase='build';
+  let objects = [];
+  const templatesLocalBaseUrl =oc.toFileUrl(path.resolve(__dirname, '../../openshift/templates'));
 
   // The building of your cool app goes here ▼▼▼
   objects = objects.concat(oc.processDeploymentTemplate(`${templatesLocalBaseUrl}/bc.yaml`, {
@@ -19,10 +19,12 @@ module.exports = (settings)=>{
       'SOURCE_REPOSITORY_URL': oc.git.http_url,
       'SOURCE_REPOSITORY_REF': oc.git.ref,
       'DOCKER_IMAGE_DIRECTORY': 'docker-images/nodejs-base',
+      'GATSBY_MATOMO_URL': 'https://matomo-devhub-prod.pathfinder.gov.bc.ca',
+      'GATSBY_MATOMO_SITE_URL': 'https://developer.gov.bc.ca',
+      'GATSBY_MATOMO_SITE_ID': '1'
     }
-  }))
+  }));
 
-
-  oc.applyRecommendedLabels(objects, phases[phase].name, phase, phases[phase].changeId, phases[phase].instance)
+  oc.applyRecommendedLabels(objects, phases[phase].name, phase, phases[phase].changeId, phases[phase].instance);
   oc.applyAndBuild(objects)
-}
+};
