866f0abcbdb9f55b1aeddce5662b78db04867e2e
Shea Phillips <shea.phillips@gmail.com>
2019-06-27 15:21:57 -0700

    Porting pipeline of Matomo to bcdk-generated version. (#737)
    * Create pipeline artificats using bcdk and customize according to component specifics.
    
    * fixing location of Jenkinsfile in 2 places so it builds the matomo component instead of devhub app.
    
    * manually updating location of pipeline file in Jenkinsfile.
    
    * making npmw executable to fix permission denied error.
    
    * adding package-lock.json as required by `npmw ci` command. note package-lock copied from app-web .pipeline.
    
    * corrected path to openshift tempaltes in build script.
    
    * explicitly settting name for maridb build
    
    * adjusting job to prevent forks from triggering builds.
    
    * Adjusting build and deploy scripts so they are properly "namespaced".
    
    * add discriminator to imagestream name.
    
    * fix typo.
    
    * fixing selector.
    
    * fixing container imagestream reference
    
    * adding imagestream object into deployment manifest - appears to be expected by `importImageStreams` function.
    
    * adding in deployment elements for matomo app.
    
    * missed files.
    
    * fix typo
    
    * fix bug in use of contact
    
    * adding matomo-proxy build elements.
    
    * adding missing SUFFIX parameter to build manifest for proxy.

---
+++ matomo/.pipeline/lib/build.js
@@ -0,0 +1,37 @@
+'use strict';
+const {OpenShiftClientX} = require('pipeline-cli')
+const path = require('path');
+
+module.exports = (settings) => {
+    const phases = settings.phases
+    const options = settings.options
+    const oc = new OpenShiftClientX(Object.assign({'namespace': phases.build.namespace}, options));
+    const phase = 'build'
+    let objects = []
+    const templatesLocalBaseUrl = oc.toFileUrl(path.resolve(__dirname, '../../openshift'))
+
+    // The building of your cool app goes here ▼▼▼
+    objects = objects.concat(oc.processDeploymentTemplate(`${templatesLocalBaseUrl}/mariadb/mariadb-build.json`, {
+        'param': {
+            'NAME': phases[phase].name,
+            'VERSION': phases[phase].tag
+        }
+    }));
+
+    objects = objects.concat(oc.processDeploymentTemplate(`${templatesLocalBaseUrl}/matomo-proxy/matomo-proxy-build.json`, {
+        'param': {
+            'NAME': phases[phase].name,
+            'VERSION': phases[phase].tag
+        }
+    }));
+
+    objects = objects.concat(oc.processDeploymentTemplate(`${templatesLocalBaseUrl}/matomo/matomo-build.json`, {
+        'param': {
+            'NAME': phases[phase].name,
+            'VERSION': phases[phase].tag
+        }
+    }));
+
+    oc.applyRecommendedLabels(objects, phases[phase].name, phase, phases[phase].changeId, phases[phase].instance)
+    oc.applyAndBuild(objects)
+}
---
+++ matomo/.pipeline/lib/deploy.js
@@ -0,0 +1,38 @@
+'use strict';
+const {OpenShiftClientX} = require('pipeline-cli')
+const path = require('path');
+
+module.exports = (settings) => {
+    const phases = settings.phases
+    const options = settings.options
+    const phase = options.env
+    const changeId = phases[phase].changeId
+    const oc = new OpenShiftClientX(Object.assign({'namespace': phases[phase].namespace}, options));
+    const templatesLocalBaseUrl = oc.toFileUrl(path.resolve(__dirname, '../../openshift'))
+    var objects = []
+
+    // The deployment of your cool app goes here ▼▼▼
+    objects = objects.concat(oc.processDeploymentTemplate(`${templatesLocalBaseUrl}/matomo-db/matomo-db-deploy.json`, {
+        'param': {
+            'NAME': phases[phase].name,
+            'SUFFIX': phases[phase].suffix,
+            'VERSION': phases[phase].tag,
+            'IMAGE_NAMESPACE': phases[phase].namespace,
+            'PERSISTENT_VOLUME_SIZE': '10Gi'
+        }
+    }));
+
+    objects = objects.concat(oc.processDeploymentTemplate(`${templatesLocalBaseUrl}/matomo/matomo-deploy.json`, {
+        'param': {
+            'NAME': phases[phase].name,
+            'SUFFIX': phases[phase].suffix,
+            'VERSION': phases[phase].tag,
+            'IMAGE_NAMESPACE': phases[phase].namespace,
+            'MATOMO_URL': 'matomo-devhub-prod.pathfinder.gov.bc.ca'
+        }
+    }));
+
+    oc.applyRecommendedLabels(objects, phases[phase].name, phase, `${changeId}`, phases[phase].instance)
+    oc.importImageStreams(objects, phases[phase].tag, phases.build.namespace, phases.build.tag)
+    oc.applyAndDeploy(objects, phases[phase].instance)
+}
