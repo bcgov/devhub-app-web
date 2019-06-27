3044a49a063a84b4609d88e4814662042949a4f4
Shea Phillips <shea.phillips@gmail.com>
2019-06-27 12:57:37 -0700

    missed files.

--- matomo/.pipeline/lib/deploy.js
+++ matomo/.pipeline/lib/deploy.js
@@ -2,27 +2,37 @@
 const {OpenShiftClientX} = require('pipeline-cli')
 const path = require('path');
 
-module.exports = (settings)=>{
-  const phases = settings.phases
-  const options= settings.options
-  const phase=options.env
-  const changeId = phases[phase].changeId
-  const oc=new OpenShiftClientX(Object.assign({'namespace':phases[phase].namespace}, options));
-  const templatesLocalBaseUrl =oc.toFileUrl(path.resolve(__dirname, '../../openshift'))
-  var objects = []
+module.exports = (settings) => {
+    const phases = settings.phases
+    const options = settings.options
+    const phase = options.env
+    const changeId = phases[phase].changeId
+    const oc = new OpenShiftClientX(Object.assign({'namespace': phases[phase].namespace}, options));
+    const templatesLocalBaseUrl = oc.toFileUrl(path.resolve(__dirname, '../../openshift'))
+    var objects = []
 
-  // The deployment of your cool app goes here ▼▼▼
-	objects = oc.processDeploymentTemplate(`${templatesLocalBaseUrl}/matomo-db/matomo-db-deploy.json`, {
-		'param':{
-			'NAME': phases[phase].name,
-			'SUFFIX': phases[phase].suffix,
-			'VERSION': phases[phase].tag,
-			'IMAGE_NAMESPACE': phases[phase].namespace,
-			'PERSISTENT_VOLUME_SIZE': '10Gi'
-		}
-	});
+    // The deployment of your cool app goes here ▼▼▼
+    objects = oc.processDeploymentTemplate(`${templatesLocalBaseUrl}/matomo-db/matomo-db-deploy.json`, {
+        'param': {
+            'NAME': phases[phase].name,
+            'SUFFIX': phases[phase].suffix,
+            'VERSION': phases[phase].tag,
+            'IMAGE_NAMESPACE': phases[phase].namespace,
+            'PERSISTENT_VOLUME_SIZE': '10Gi'
+        }
+    });
 
-  oc.applyRecommendedLabels(objects, phases[phase].name, phase, `${changeId}`, phases[phase].instance)
-  oc.importImageStreams(objects, phases[phase].tag, phases.build.namespace, phases.build.tag)
-  oc.applyAndDeploy(objects, phases[phase].instance)
+    objects.contact(oc.processDeploymentTemplate(`${templatesLocalBaseUrl}/matomo/matomo-deploy.json`, {
+        'param': {
+            'NAME': phases[phase].name,
+            'SUFFIX': phases[phase].suffix,
+            'VERSION': phases[phase].tag,
+            'IMAGE_NAMESPACE': phases[phase].namespace,
+            'MATOMO_URL': 'matomo-devhub-prod.pathfinder.gov.bc.ca'
+        }
+    }));
+
+    oc.applyRecommendedLabels(objects, phases[phase].name, phase, `${changeId}`, phases[phase].instance)
+    oc.importImageStreams(objects, phases[phase].tag, phases.build.namespace, phases.build.tag)
+    oc.applyAndDeploy(objects, phases[phase].instance)
 }
