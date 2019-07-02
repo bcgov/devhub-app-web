9ba2fadab814867983e1478a1299566cc6c702c2
DerekSgov <50340998+DerekSgov@users.noreply.github.com>
2019-07-02 16:16:17 -0700

    Adding node fields to MarkdownRemark Nodes (#750)
    * Adding node fields to MarkdownRemark Nodes, with resonable defaults/fallbacks
    
    * Refactor ternary statement into if so that it is more readable

--- app-web/gatsby/onCreateNode.js
+++ app-web/gatsby/onCreateNode.js
@@ -16,6 +16,9 @@ limitations under the License.
 Created by Patrick Simonian
 */
 const htmlToFormattedText = require('html-to-formatted-text');
+const { isArray, isString } = require('lodash');
+const visit = require('unist-util-visit');
+const remark = require('remark');
 
 module.exports = ({ node, actions }) => {
   const { createNodeField } = actions;
@@ -37,6 +40,60 @@ module.exports = ({ node, actions }) => {
       node,
       name: 'location',
       value: node.venue ? node.venue.address_1 : '',
+    });
+  } else if (node.internal.type === 'MarkdownRemark') {
+    let title = node.frontmatter.title;
+    const ast = remark.parse(node.internal.content);
+    //if our title is blank, visit will search through the content for a usable and reasonable title
+    visit(ast, 'heading', node => {
+      // is title blank and is node on first line and a h1 or h2?
+      if (title === '' && (node.depth === 1 || node.depth === 2)) {
+        // accept headers up to 3rd line of markdown file
+        if (node.position.start.line < 3) {
+          title = node.children[0].value;
+        }
+      }
+    });
+
+    let labels = [];
+    // assert the shape of labels in frontmatter
+    if (Object.prototype.hasOwnProperty(node.frontmatter, 'labels')) {
+      if (isArray(node.frontmatter.labels) && node.frontmatter.every(isString)) {
+        labels = node.frontmatter.labels;
+      } else if (isString(node.frontmatter.labels)) {
+        // split by any commas and trim
+        labels = node.frontmatter.labels.split().map(text => text.trim());
+      }
+    }
+
+    createNodeField({
+      node,
+      name: 'title',
+      value: node.frontmatter.title ? node.frontmatter.title : title,
+    });
+
+    createNodeField({
+      node,
+      name: 'description',
+      value: node.frontmatter.description ? node.frontmatter.description : '',
+    });
+
+    createNodeField({
+      node,
+      name: 'image',
+      value: node.frontmatter.image ? node.frontmatter.image : '',
+    });
+
+    createNodeField({
+      node,
+      name: 'labels',
+      value: labels,
+    });
+
+    createNodeField({
+      node,
+      name: 'author',
+      value: node.frontmatter.author ? node.frontmatter.author : '',
     });
   }
 };
