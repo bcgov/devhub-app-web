b6b595995bc56096ad81cc5395b407ead60f3522
Shea Phillips <shea.phillips@gmail.com>
2019-07-02 21:25:46 -0700

    adding DEV "environment" for matomo prior to version update

--- matomo/.pipeline/lib/config.js
+++ matomo/.pipeline/lib/config.js
@@ -5,8 +5,36 @@ const version = '1.0.0'
 const name = 'matomo'
 
 const phases = {
-  build: {namespace:'devhub-tools'    , name: `${name}`, phase: 'build'  , changeId:changeId, suffix: `-build-${changeId}`  , instance: `${name}-build-${changeId}`  , version:`${version}-${changeId}`, tag:`build-${version}-${changeId}`},
-  prod: {namespace:'devhub-tools'    , name: `${name}`, phase: 'prod'  , changeId:changeId, suffix: `-prod`  , instance: `${name}-prod`  , version:`${version}`, tag:`prod-${version}`},
+    build: {
+        namespace: 'devhub-tools',
+        name: `${name}`,
+        phase: 'build',
+        changeId: changeId,
+        suffix: `-build-${changeId}`,
+        instance: `${name}-build-${changeId}`,
+        version: `${version}-${changeId}`,
+        tag: `build-${version}-${changeId}`
+    },
+    dev: {
+        namespace: 'devhub-tools',
+        name: `${name}`,
+        phase: 'dev',
+        changeId: changeId,
+        suffix: `-dev-${changeId}`,
+        instance: `${name}-dev-${changeId}`,
+        version: `${version}-${changeId}`,
+        tag: `dev-${version}-${changeId}`
+    },
+    prod: {
+        namespace: 'devhub-tools',
+        name: `${name}`,
+        phase: 'prod',
+        changeId: changeId,
+        suffix: `-prod`,
+        instance: `${name}-prod`,
+        version: `${version}`,
+        tag: `prod-${version}`
+    },
 };
 
 // This callback forces the node process to exit as failure.
