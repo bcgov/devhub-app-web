8c4421e9740394b36baff8d9b90ecb502eb43fde
Patrick Simonian <Patrick.Simonian@gov.bc.ca>
2019-07-10 13:57:05 -0700

    linted

--- .pipeline/lib/build-previewer.js
+++ .pipeline/lib/build-previewer.js
@@ -1,28 +1,36 @@
 'use strict';
-const {OpenShiftClientX} = require('pipeline-cli');
+const { OpenShiftClientX } = require('pipeline-cli');
 const path = require('path');
 
-module.exports = (settings)=>{
+module.exports = settings => {
   const phases = settings.phases;
   const options = settings.options;
-  const oc=new OpenShiftClientX(Object.assign({'namespace':phases.build.namespace}, options));
-  const phase='prod';
+  const oc = new OpenShiftClientX(Object.assign({ namespace: phases.build.namespace }, options));
+  const phase = 'prod';
   let objects = [];
   const templatesLocalBaseUrl = oc.toFileUrl(path.resolve(__dirname, '../../openshift/templates'));
 
   // The building of your cool app goes here ▼▼▼
-  objects = objects.concat(oc.processDeploymentTemplate(`${templatesLocalBaseUrl}/devhub-previewer.bc.yaml`, {
-    'param':{
-      'NAME': phases[phase].name,
-      'SUFFIX': phases[phase].suffix,
-      'VERSION': phases[phase].tag,
-      'SOURCE_REPOSITORY_URL': oc.git.http_url,
-      'SOURCE_REPOSITORY_REF': oc.git.ref,
-      'DOCKER_IMAGE': 'registry.hub.docker.com/bcgovimages/devhub-previewer:latest',
-      'DOCKERHUB_SECRET': 'dockerhub'
-    }
-  }));
+  objects = objects.concat(
+    oc.processDeploymentTemplate(`${templatesLocalBaseUrl}/devhub-previewer.bc.yaml`, {
+      param: {
+        NAME: phases[phase].name,
+        SUFFIX: phases[phase].suffix,
+        VERSION: phases[phase].tag,
+        SOURCE_REPOSITORY_URL: oc.git.http_url,
+        SOURCE_REPOSITORY_REF: oc.git.ref,
+        DOCKER_IMAGE: 'registry.hub.docker.com/bcgovimages/devhub-previewer:latest',
+        DOCKERHUB_SECRET: 'dockerhub',
+      },
+    }),
+  );
 
-  oc.applyRecommendedLabels(objects, phases[phase].name, phase, phases[phase].changeId, phases[phase].instance);
-  oc.applyAndBuild(objects)
+  oc.applyRecommendedLabels(
+    objects,
+    phases[phase].name,
+    phase,
+    phases[phase].changeId,
+    phases[phase].instance,
+  );
+  oc.applyAndBuild(objects);
 };
