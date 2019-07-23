8a41cf7ab05cdfffd44d5eca2a87e4826cf8167f
DerekSgov <Derek.Siemens@gov.bc.ca>
2019-07-23 15:29:10 -0700

    Setting up Apollo search component

---
+++ app-web/src/components/Search/Search.js
@@ -0,0 +1,40 @@
+/*
+Copyright 2019 Province of British Columbia
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at 
+
+   http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+Created by Derek Siemens
+*/
+/*
+import React from 'react';
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "react-apollo";
+//run time query
+import gql from 'graphql-tag';
+
+//this needs to be an enviroment variable later on......
+const client = new ApolloClient({
+    uri: "https://rocketGate/Devhub.pathfinder.gov.bc.ca"
+  });
+
+
+const devHubQuery = gql`
+{}`;
+
+const SearchApollo = () => {
+<ApolloProvider clinet={client} >
+
+</ApolloProvider>
+
+
+};*/
