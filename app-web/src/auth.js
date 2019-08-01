88f9e52d6f311aeb7b55c0cf8784d6fd6f2f03cc
Patrick Simonian <Patrick.Simonian@gov.bc.ca>
2019-07-31 18:48:16 -0700

    fix a bug where auth doesn't respect redirects

--- app-web/src/auth.js
+++ app-web/src/auth.js
@@ -2,18 +2,18 @@ import { ImplicitAuthManager } from '@bcgov/common-web-utils';
 
 import { SSO_BASE_URL, SSO_CLIENT_ID, SSO_REALM_NAME } from './constants/api';
 
-const config = {
-  baseURL: SSO_BASE_URL,
-  clientId: SSO_CLIENT_ID,
-  realmName: SSO_REALM_NAME,
-};
-
 let iam = null;
 
 /**
  * returns a singleton of the implicit auth manager
  */
 export const createIam = () => {
+  const config = {
+    baseURL: SSO_BASE_URL,
+    clientId: SSO_CLIENT_ID,
+    realmName: SSO_REALM_NAME,
+    redirectURI: window.location.origin + window.location.pathname,
+  };
   if (iam === null) {
     iam = new ImplicitAuthManager(config);
     Object.freeze(iam);
