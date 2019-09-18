17ff9ca25ef596b575b04bdacf066ae83a4c5497
Patrick Simonian <Patrick.Simonian@gov.bc.ca>
2019-09-18 10:12:16 -0700

    add authentication check back to search

--- app-web/src/utils/hooks.js
+++ app-web/src/utils/hooks.js
@@ -152,7 +152,7 @@ export const useSearchGate = (authenticated, queryString, client) => {
   useEffect(() => {
     setLoading(loading);
 
-    if (!queryString) {
+    if (!authenticated || !queryString) {
       setResults([]);
     } else if (!_loading) {
       setResults(data.search);
