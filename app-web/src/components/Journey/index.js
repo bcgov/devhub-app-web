fc444f447bb9b0f92443dd7701d85e18ea6c5929
Patrick Simonian <Patrick.Simonian@gov.bc.ca>
2019-10-09 09:29:21 -0700

    complete visual testing of journey map

--- app-web/src/components/Journey/index.js
+++ app-web/src/components/Journey/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import { Link } from '../UI/Link';
+import { ResourceTypeIcon } from '../UI/ResourceTypeIcon';
 import PropTypes from 'prop-types';
 import styled from '@emotion/styled';
+import isFunction from 'lodash/isFunction';
 
 const Container = styled.div`
   position: relative;
-  width: 100px;
-  height: 100px;
   display: flex;
   justify-content: center;
   align-items: center;
@@ -17,6 +18,7 @@ const stationVariants = ['up', 'down'];
 
 const Circle = styled.span`
   flex-shrink: 0;
+  cursor: pointer;
   position: relative;
   display: inline-block;
   transition: all 0.15s ease-out;
@@ -39,35 +41,160 @@ const Circle = styled.span`
 
 const Name = styled.span`
   position: absolute;
-  width: 100%;
-  padding-left: 5px;
+  width: 150px;
   top: ${({ variant }) => (variant === 'up' ? `initial` : `50%`)};
+  left: 50%;
   bottom: ${({ variant }) => (variant === 'down' ? `initial` : `50%`)};
   transform: ${({ variant, size }) =>
-    variant === 'up' ? `translateY(-${size - 5}px)` : `translateY(${size - 5}px)`};
+      variant === 'up' ? `translateY(-${size - 5}px)` : `translateY(${size - 5}px)`}
+    translateX(-50%);
   color: ${({ theme }) => theme.colors.darkgrey};
 `;
 
-export const Station = ({ name, variant, color, size = 25, ...rest }) => {
+const AuxillaryField = styled.span`
+  position: absolute;
+  top: ${({ variant }) => (variant === 'down' ? `initial` : `50%`)};
+  left: 50%;
+  bottom: ${({ variant }) => (variant === 'up' ? `initial` : `50%`)};
+  transform: ${({ variant, size }) =>
+      variant === 'down' ? `translateY(-${size - 5}px)` : `translateY(${size - 5}px)`}
+    translateX(-50%);
+`;
+
+export const Station = ({ name, variant, color, size = 25, render, ...rest }) => {
   const fontSize = name.length > 24 ? 14 : 16;
 
   return (
-    <Container data-name={name} {...rest}>
-      <Name variant={variant} style={{ fontSize }} size={size}>
-        {name}
-      </Name>{' '}
+    <Container {...rest}>
       <Circle color={color} size={size} />
+      <Name variant={variant} style={{ fontSize }} size={size}>
+        {isFunction(name) ? name() : name}
+      </Name>{' '}
+      {render && (
+        <AuxillaryField variant={variant} size={size}>
+          {render()}
+        </AuxillaryField>
+      )}
     </Container>
   );
 };
 
 Station.propTypes = {
-  name: PropTypes.string,
+  name: PropTypes.oneOfType([PropTypes.func, PropTypes.string]),
   variant: PropTypes.oneOf(stationVariants),
+  color: PropTypes.string,
+  size: PropTypes.number,
+  render: PropTypes.func,
+};
+
+Station.defaultProps = {
+  variant: 'up',
+};
+
+const Line = styled.div`
+  height: ${({ size }) => 0.5 * size}px;
+  width: calc(100% - ${({ size }) => size}px);
+  position: absolute;
+  top: 50%;
+  transform: translateY(-50%) translateX(${({ size }) => size * 0.5}px);
+  background-color: ${({ color, theme }) =>
+    color && theme.colors[color] ? theme.colors[color] : theme.colors.blue};
+`;
+
+const Subway = styled.div`
+  display: flex;
+  position: relative;
+  align-items: center;
+  padding: 100px 0;
+  justify-content: space-between;
+`;
+
+export const SubwayLine = ({ stops, color, size = 25 }) => (
+  <Subway>
+    <Line color={color} size={size} />
+    {stops.map((stop, index) => {
+      const variant = index % 2 === 0 ? 'up' : 'down';
+      const extraProps = {};
+      if (stop.connectsWith) {
+        extraProps.render = () => (
+          <JunctionList
+            variant={variant === 'up' ? 'down' : 'up'}
+            style={{ transform: 'translateX(calc(50% - 2px))' }}
+            links={stop.connectsWith.map(({ name, path, resourceType }) => ({
+              name,
+              to: path,
+              resourceType,
+            }))}
+          />
+        );
+      }
+      return (
+        <Station
+          key={stop.name}
+          name={stop.name}
+          size={size}
+          variant={variant}
+          color={color}
+          {...extraProps}
+        />
+      );
+    })}
+  </Subway>
+);
+
+SubwayLine.propTypes = {
+  stops: PropTypes.arrayOf(PropTypes.shape({ name: PropTypes.string })),
   color: PropTypes.string,
   size: PropTypes.number,
 };
 
-Station.defaultProps = {
+const JunctionContainer = styled.ul`
+  margin: 5px 0;
+  border-left: 2px solid ${({ theme }) => theme.colors.darkgrey};
+  color: ${({ theme }) => theme.colors.darkgrey};
+  font-size: 12px;
+  font-weight: 400;
+  border-top-left-radius: ${({ variant }) => (variant === 'up' ? '4px' : 0)};
+  border-bottom-left-radius: ${({ variant }) => (variant === 'down' ? '4px' : 0)};
+  padding: ${({ variant }) => (variant === 'down' ? '4px 0 0 4px' : '0 0 4px 4px')};
+  list-style: none;
+  li {
+    margin: 6px 0;
+
+    transform: ${({ variant }) => (variant === 'up' ? 'translateY(-0.5em)' : 'translateY(0.5em)')};
+    display: flex;
+    align-items: flex-end;
+    justify-content: flex-start;
+    text-align: left;
+    a {
+      max-width: 150px;
+      flex: 1 0 auto;
+      color: ${({ theme }) => theme.colors.lightblue};
+    }
+  }
+`;
+
+export const JunctionList = ({ links, variant, ...rest }) => {
+  return (
+    <JunctionContainer variant={variant} {...rest}>
+      {links.map(l => (
+        <li key={l.to}>
+          <span>
+            ─&nbsp;
+            <ResourceTypeIcon type={l.resourceType} style={{ marginRight: '4px' }} />
+          </span>
+          <Link to={l.to}>{l.name}</Link>
+        </li>
+      ))}
+    </JunctionContainer>
+  );
+};
+
+JunctionList.propTypes = {
+  links: PropTypes.arrayOf(PropTypes.shape({ name: PropTypes.string, to: PropTypes.string })),
+  variant: PropTypes.oneOf(stationVariants),
+};
+
+JunctionList.defaultProps = {
   variant: 'up',
 };
--- app-web/stories/journey.stories.js
+++ app-web/stories/journey.stories.js
@@ -1,31 +1,152 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { storiesOf } from '@storybook/react';
-import { Station } from '../src/components/Journey';
+import { Station, SubwayLine, JunctionList } from '../src/components/Journey';
 
 storiesOf('Journeys', module)
   .add('A Station', () => <Station name="edmonds" variant="up" />)
   .add('A Station (with custom color)', () => <Station color="green" name="edmonds" variant="up" />)
   .add('A Station (with down variant)', () => <Station color="red" name="edmonds" variant="down" />)
   .add('A Station (with long name, variant down)', () => (
-    <Station
-      color="red"
-      name="The Meaning of Life and Everything Is not 42 (big surprise)"
-      variant="down"
-    />
-  ))
-  .add('A Station (with long name, variant up)', () => (
-    <Station color="red" name="The Meaning of Life and Everything Is not 42 (big surprise)" />
-  ))
-  .add('Stations side by side', () => (
-    <div style={{ display: 'flex', height: '500px', alignItems: 'center' }}>
+    <React.Fragment>
+      <br />
       <Station
         color="red"
         name="The Meaning of Life and Everything Is not 42 (big surprise)"
         variant="down"
       />
-      <Station color="blue" name="Nanaimo" variant="up" />
-      <Station color="red" name="Patterson" variant="down" />
-      <Station color="green" name="Metrotown" variant="up" />
+    </React.Fragment>
+  ))
+  .add('A Station (with long name, variant up)', () => (
+    <React.Fragment>
+      <Station color="red" name="The Meaning of Life and Everything Is not 42 (big surprise)" />
+    </React.Fragment>
+  ))
+  .add('Station with name as render prop', () => (
+    <React.Fragment>
+      <Station
+        color="red"
+        variant="down"
+        name={() => {
+          const [toggled, setToggled] = useState(false);
+          return (
+            <div>
+              <button onClick={() => setToggled(!toggled)}>click me!</button>
+              {toggled && <p>AWH YEAH</p>}
+            </div>
+          );
+        }}
+      />
+    </React.Fragment>
+  ))
+  .add('Station with Auxillary field, (leveraing renderProp)', () => (
+    <div style={{ display: 'flex', height: '500px', alignItems: 'center', width: '500px' }}>
+      <Station
+        style={{ width: 150 }}
+        color="red"
+        name="The Meaning of Life and Everything Is not 42 (big surprise)"
+        render={() => <p>aux</p>}
+      />
+      <Station
+        style={{ width: 150 }}
+        color="red"
+        variant="down"
+        name="The Meaning of Life and Everything Is not 42 (big surprise)"
+        render={() => <p>aux</p>}
+      />
+    </div>
+  ))
+  .add('Stations side by side', () => (
+    <div style={{ display: 'flex', height: '500px', alignItems: 'center', width: '500px' }}>
+      <Station
+        color="red"
+        name="The Meaning of Life and Everything Is not 42 (big surprise)"
+        variant="down"
+        style={{ width: 150 }}
+      />
+      <Station color="blue" name="Nanaimo" variant="up" style={{ width: 150 }} />
+      <Station color="red" name="Patterson" variant="down" style={{ width: 150 }} />
+      <Station color="green" name="Metrotown" variant="up" style={{ width: 150 }} />
+    </div>
+  ))
+  .add('Subway Line', () => (
+    <div style={{ padding: '50px', width: '550px' }}>
+      <SubwayLine
+        stops={[
+          { name: 'Yaletown Roundhouse' },
+          { name: 'Olympic Village' },
+          { name: 'Broadway City Hall' },
+          { name: 'King Edward' },
+          { name: 'Oakridge 41st' },
+          { name: 'Oakridge 41st' },
+        ]}
+        color="green"
+      />
+    </div>
+  ))
+  .add('Junction List', () => (
+    <div style={{ padding: '50px', width: '550px' }}>
+      <JunctionList
+        links={[
+          { name: 'foo', to: 'text' },
+          { name: 'foo', to: 'text' },
+          { name: 'foo', to: 'text' },
+        ]}
+      />
+      <JunctionList
+        variant="down"
+        links={[
+          { name: 'foo', to: 'text' },
+          { name: 'foo', to: 'text' },
+          { name: 'foo', to: 'text' },
+        ]}
+      />
+    </div>
+  ))
+  .add('Subway with Junctions Noted', () => (
+    <div style={{ padding: '50px', width: '50%' }}>
+      <SubwayLine
+        stops={[
+          { name: 'Yaletown Roundhouse' },
+          { name: 'Olympic Village' },
+          {
+            name: 'Broadway City Hall',
+            connectsWith: [
+              { resourceType: 'Topics', name: 'billy', path: '/topic' },
+              { resourceType: 'Topics', name: 'is cool', path: '/topic' },
+              {
+                resourceType: 'Topics',
+                name: "isn't that right? wouldn't you say?",
+                path: '/topic',
+              },
+              {
+                resourceType: 'Topics',
+                name: "isn't that right? wouldn't you say?",
+                path: '/topic',
+              },
+            ],
+          },
+          {
+            name: 'King Edward',
+            connectsWith: [
+              { resourceType: 'Topics', name: 'foo', path: '/topic' },
+              { resourceType: 'Topics', name: 'foo', path: '/topic' },
+            ],
+          },
+          { name: 'Oakridge 41st' },
+          {
+            name: 'Langara 49th',
+            connectsWith: [
+              { resourceType: 'Topics', name: 'foo', path: '/topic' },
+              { resourceType: 'Topics', name: 'foo', path: '/topic' },
+              { resourceType: 'Topics', name: 'foo', path: '/topic' },
+            ],
+          },
+          {
+            name: 'Marine Drive',
+          },
+        ]}
+        color="lightblue  "
+      />
     </div>
   ));
