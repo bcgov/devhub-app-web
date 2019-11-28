f745bb6e72fda6d8bb698293ffc2c152339438d6
w8896699 <billy.li@gov.bc.ca>
2019-11-28 09:17:31 -0800

    google verification

--- app-web/src/components/SEO/SEO.js
+++ app-web/src/components/SEO/SEO.js
@@ -2,23 +2,7 @@ import React from 'react';
 import { Helmet } from 'react-helmet';
 import PropTypes from 'prop-types';
 
-export const SEO = ({ title }) => (
-  <Helmet title={title}>
-    <script type="application/ld+json">
-      {`
-        '@context': 'https://billymagic.org',
-        '@type': 'billyproject',
-        url: 'http://www.billyisthebest.com',
-        name: 'billy technologies',
-        contactPoint: {
-          '@type': 'ContactPoint',
-          telephone: '+1-222-3333-4444',
-          contactType: 'Billy Support',
-        },
-      `}
-    </script>
-  </Helmet>
-);
+export const SEO = ({ title }) => <Helmet title={title}></Helmet>;
 
 SEO.propTypes = {
   title: PropTypes.string,
