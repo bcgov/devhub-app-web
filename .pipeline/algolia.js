f0ae038e307c21f6a1f812abbd9a4f61b727716e
Patrick Simonian <Patrick.Simonian@gov.bc.ca>
2019-12-03 16:24:19 -0800

    Add delete algolia index method
    remove log

--- .pipeline/algolia.js
+++
@@ -1,5 +0,0 @@
-'use strict';
-const task = require('./lib/algolia.js');
-const settings = require('./lib/config.js');
-
-task(settings);
--- .pipeline/lib/algolia.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-require('dotenv').config();
-const { cloneIndexTo } = require('../utils');
-
-module.exports = async settings => {
-  const { env, suffix } = settings.options;
-
-  return await cloneIndexTo(`Devhub-Algolia-${suffix}`, `Devhub-Algolia-${env}`);
-};
