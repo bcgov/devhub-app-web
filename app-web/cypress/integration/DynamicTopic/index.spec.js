73d7363686877bcd162ac56e6e730281a9b32af4
Patrick Simonian <Patrick.Simonian@gov.bc.ca>
2019-12-13 15:47:30 -0800

    make it only for prs

--- app-web/cypress/integration/DynamicTopic/index.spec.js
+++ app-web/cypress/integration/DynamicTopic/index.spec.js
@@ -1,14 +1,10 @@
 describe('Dynamic Topic Specs', () => {
   describe('Arriving on the popular page', () => {
-    it('has a github issue link at the bottom of the first page', () => {
+    it('can visit featured topics', () => {
       cy.visit('/');
-      cy.getByTestId('topic-Popular').click();
+      cy.contains('a', 'Featured').click();
       cy.log('checking url matches');
-      cy.url().should('contain', '/topic/popular');
-      cy.log('finding create issue link');
-      cy.getByTestId('actions-issue')
-        .should('have.attr', 'href')
-        .and('match', /https:\/\/(www\.)?github.com\/bcgov\/\w+/);
+      cy.url().should('contain', '/topic/featured');
     });
   });
 });
