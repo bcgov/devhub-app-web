8f733f8b3e366d99543e1e9ba226a1c15ac243c2
Patrick Simonian <Patrick.Simonian@gov.bc.ca>
2019-12-27 15:55:26 -0800

    update linting rules

--- .github/actions/devhub-markdown-frontmatter-verify/src/utils.js
+++ .github/actions/devhub-markdown-frontmatter-verify/src/utils.js
@@ -2,21 +2,23 @@
  * reduces the result from the contents graphql query into a list
  * @param {String} path the raw contents query
  * @param {Array} results the raw contents query
- * @returns {Array} [{...fileContents}] 
+ * @returns {Array} [{...fileContents}]
  */
-const reduceContentsResults = (path, results) => results.repository.object.entries.map(entry => ({
-  contents: entry.object.text,
-  path: `${path}/${entry.name}`
-}));
+const reduceContentsResults = (path, results) =>
+  results.repository.object.entries.map(entry => ({
+    contents: entry.object.text,
+    path: `${path}/${entry.name}`,
+  }));
 
-const reduceResultsToData = results => results.map(r => {
-  return JSON.parse(r.contents)
-});
+const reduceResultsToData = results =>
+  results.map(r => {
+    return JSON.parse(r.contents);
+  });
 
-const reduceFileResults = (result) => result.repository.object.text
+const reduceFileResults = result => result.repository.object.text;
 
 module.exports = {
   reduceContentsResults,
   reduceResultsToData,
   reduceFileResults,
-}
+};
