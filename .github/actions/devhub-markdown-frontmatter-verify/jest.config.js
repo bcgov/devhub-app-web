8cbd2d86d0e839a9113b57e180dcc247718a8b56
Patrick Simonian <Patrick.Simonian@gov.bc.ca>
2019-12-27 16:39:13 -0800

    update workflow

---
+++ .github/actions/devhub-markdown-frontmatter-verify/jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  globals: {
+    __PATH_PREFIX__: '',
+  },
+  transform: {
+    '.(js|jsx)': 'babel-jest',
+  },
+  testPathIgnorePatterns: ['/node_modules/', '/.cache/', '/cypress/'],
+  moduleFileExtensions: ['js', 'module.css', '.css', 'json']
+};
