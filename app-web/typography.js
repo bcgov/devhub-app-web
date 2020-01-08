6557ce0aa2877a2998cf75aed184e9ca8f8fbdfa
Jaskaren <jas@Jaskarens-MacBook-Pro.local>
2020-01-08 11:12:59 -0800

    Added BC Sans Font

--- app-web/typography.js
+++ app-web/typography.js
@@ -1,10 +1,11 @@
 import Typography from 'typography';
+import '@bcgov/bc-sans/css/BCSans.css';
 
 const typography = new Typography({
-  baseFontSize: '18px',
+  baseFontSize: '16px',
   baseLineHeight: 1.25,
-  headerFontFamily: ['Noto Sans', 'Verdana', 'Arial', 'sans-serif'],
-  bodyFontFamily: ['Noto Sans', 'Verdana', 'Arial', 'sans-serif'],
+  headerFontFamily: ['BCSans', 'Noto Sans', 'Verdana', 'Arial', 'sans-serif'],
+  bodyFontFamily: ['BCSans', 'Noto Sans', 'Verdana', 'Arial', 'sans-serif'],
   scaleRatio: 2.074,
 });
 
