0751fd0b14b6128a1785a6e84ef7d60af28e2758
Saini <jsaini@ncm050119.idir.bcgov>
2020-01-16 11:03:45 -0800

    Added component

---
+++ app-web/src/components/UI/AlgoliaBranding/AlgoliaBranding.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import Aux from '../../../hoc/auxillary';
+import logo from '../../../assets/images/algolia.png';
+import { css } from '@emotion/core';
+import styled from '@emotion/styled';
+
+const noMargin = css`
+  margin-bottom: 0;
+`;
+
+const SmallLogo = styled.img`
+  ${noMargin}
+  width: 50px;
+  @media (min-width: 480px) {
+    display: none;
+  }
+`;
+
+const AlgoliaLogo = () => (
+  <Aux>
+    <SmallLogo src={logo} alt="Algolia" />
+  </Aux>
+);
+
+export default AlgoliaLogo;
