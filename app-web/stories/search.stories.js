4bfb3aa0b5aada2976cfb21e64a7a90a20af2257
jas29 <jas29@github.com>
2020-01-17 12:16:23 -0800

    Removing comments

--- app-web/stories/search.stories.js
+++ app-web/stories/search.stories.js
@@ -2,19 +2,6 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import AlgoliaLogo from '../src/components/UI/AlgoliaBranding/AlgoliaBranding';
 
-// const contents = [
-//   {
-//     title: 'Foo',
-//     resourceType: 'Documentation',
-//     path: '/foo',
-//   },
-//   {
-//     title: 'External Foo',
-//     resourceType: 'Components',
-//     path: 'https://external.foo',
-//   },
-// ];
-
 storiesOf('Search Branding', module)
   .add('Algolia brand', () => <AlgoliaLogo title="Search" />)
   .add('Branding with border', () => (
