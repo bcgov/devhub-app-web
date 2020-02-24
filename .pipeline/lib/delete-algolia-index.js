a19fc47ed8ba06f5fb365ed28135b39acfa0495e
Patrick Simonian <Patrick.Simonian@gov.bc.ca>
2020-02-24 15:13:18 -0800

    fix delete index  func

--- .pipeline/lib/delete-algolia-index.js
+++ .pipeline/lib/delete-algolia-index.js
@@ -5,8 +5,9 @@ const algoliasearch = require('algoliasearch');
 module.exports = async settings => {
   const client = algoliasearch(process.env.ALGOLIA_APP_ID, process.env.ALGOLIA_ADMIN_KEY);
   const { suffix } = settings.options;
-  const index = `Devhub-Algolia-${suffix}`;
-  console.log(`Deleting Algolia Index ${index}`);
+  const index = client.initIndex(`Devhub-Algolia-${suffix}`);
 
-  return await client.index.delete();
+  console.log(`Deleting Algolia Index ${suffix}`);
+
+  return await index.delete();
 };
