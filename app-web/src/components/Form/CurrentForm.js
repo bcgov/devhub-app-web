ee7a063b9d3605e583feae5e87377210efb61cb7
jas29 <karansaini29@gmail.com>
2020-04-20 14:17:38 -0700

    Added intitial form

--- app-web/src/components/Form/CurrentForm.js
+++ app-web/src/components/Form/CurrentForm.js
@@ -1,38 +1,85 @@
-import React from 'react';
+import {React, useMemo, useState, Fragment} from 'react';
 import { Formik, Form, useField } from "formik";
 import * as Yup from "yup";
-import {MyTextInput,MyCheckbox,MySelect} from './form'
+import {MyTextInput,MyCheckbox,MySelect,StyledSelect} from './form'
+import { graphql, useStaticQuery } from 'gatsby';
+import { flattenGatsbyGraphQL } from '../../utils/dataHelpers'
 
 
-const CurrentForm = ({}) => (
-      <Formik
-        initialValues={{
-          jobType: "" // added for our select
-        }}
-        validationSchema={Yup.object({
-          jobType: Yup.string()
-            .oneOf(
-              ["addToTopic", "addToJourney"],
-              "Invalid Job Type"
-            )
-            .required("Required")
-        })}
-        onSubmit={(values, { setSubmitting }) => {
-          setTimeout(() => {
-            alert(JSON.stringify(values, null, 2));
-            setSubmitting(false);
-          }, 400);
-        }}
-      >
-        <Form>
-          <MySelect label="Would you like to add to an existing Topic or Journey" name="jobType">
+export const CurrentForm = () => {
+  const [currVal, setVal] = useState("")
+  let content;
+  
+  if(currVal === "addToTopic"){
+    content = (<DataQuery datatype="topics"></DataQuery>)
+  }
+  else if(currVal === "addToJourney"){
+    content=(<DataQuery datatype="journeys"></DataQuery>)
+  }
+
+  return (
+    <Fragment>
+      <form>
+          <StyledSelect label="Would you like to add to an existing Topic or Journey" name="jobType" onChange={e => {setVal(e.target.value)}}>
             <option value="">Select a job type</option>
             <option value="addToTopic">Topic</option>
             <option value="addToJourney">Journey</option>
-          </MySelect>
-          <button type="submit">Submit</button>
-        </Form>
-      </Formik>
-);
+          </StyledSelect>
+        </form>
+        {content}
+    </Fragment>
+  );
+};
 
 export default CurrentForm;
+
+const DataQuery = ({datatype}) => {
+
+  const {topics,journeys} = useStaticQuery(graphql`
+  query {
+    topics: allTopicRegistryJson {
+      edges {
+        node {
+          id
+          name
+          description
+        }
+      }
+    }
+    journeys: allJourneyRegistryJson {
+      edges {
+        node {
+          id
+          name
+          fields {
+            slug
+            description
+          }
+        }
+      }
+    }
+  }
+`);
+let data;
+if (datatype === "topics"){
+  data = useMemo(() => flattenGatsbyGraphQL(topics.edges), [
+    topics.edges,
+  ]);
+}
+else if (datatype === "journeys"){
+  data = useMemo(() => flattenGatsbyGraphQL(journeys.edges), [
+    journeys.edges,
+  ]);
+}
+
+return(
+  <form>
+    <StyledSelect>
+      {data.map(topic => (
+        <option value={topic.name} key={topic.id}>{topic.name}</option>
+      ))}
+    </StyledSelect>
+  </form>
+);
+}
+
--- app-web/src/pages/contentContribution.js
+++ app-web/src/pages/contentContribution.js
@@ -7,7 +7,7 @@ import * as Yup from "yup";
 import { SEO } from '../components/SEO/SEO';
 import { Title } from '../components/Page';
 import Main from '../components/Page/Main';
-import {MyTextInput,MyCheckbox,MySelect} from '../components/Form/form'
+import {MyTextInput,MyCheckbox,MySelect,StyledSelect} from '../components/Form/form'
 import CurrentForm from '../components/Form/CurrentForm'
 
 
@@ -15,14 +15,10 @@ import CurrentForm from '../components/Form/CurrentForm'
 export const contentContribution = ({}) => {
 
     const [currentVal, setVal] = useState("");
-    let form;
-    useEffect(() => {
-    console.log("here",currentVal)
+    let content;
     if(currentVal === "addToExistingResource"){
-        console.log("here true")
-        form = <CurrentForm></CurrentForm>
+        content = <CurrentForm></CurrentForm>
     }
-    })
     return (
         <Layout>
         <SEO title="Content Contribution" />
@@ -31,29 +27,15 @@ export const contentContribution = ({}) => {
           title="Content Contribution"
           subtitle="Want to add content to the Devhub? Let's answer some questions first"
         />
-        <Formik
-        initialValues={{
-          jobType: "" // added for our select
-        }}
-        validationSchema={Yup.object({
-          jobType: Yup.string()
-            .oneOf(
-              ["addToExistingResource", "addNewTopic", "addNewJourney"],
-              "Invalid Job Type"
-            )
-            .required("Required")
-        })}
-      >
-        <Form> 
-          <MySelect label="How would you like to contribute ?" name="value" onChange={e => {setVal(e.target.value)}}>
+        <form> 
+          <StyledSelect label="How would you like to contribute ?" onChange={e => {setVal(e.target.value)}}>
             <option value="">Select a job type</option>
             <option value="addToExistingResource">Add content to existing Topic/Journey</option>
             <option value="addNewTopic">Add a new Topic</option>
             <option value="addNewJourney">Add a new Journey</option>
-          </MySelect>
-        </Form>
-      </Formik>
-      {form}
+          </StyledSelect>
+        </form>
+      {content}
         </Main>
     </Layout>
     );
