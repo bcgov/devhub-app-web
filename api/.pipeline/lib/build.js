87430a36840ff4c802ebb57bf832b418bffee0eb
Patrick Simonian <Patrick.Simonian@gov.bc.ca>
2020-04-29 15:32:16 -0700

    update pipeline scripts

--- api/.pipeline/lib/build.js
+++ api/.pipeline/lib/build.js
@@ -11,7 +11,16 @@ module.exports = settings => {
   const templatesLocalBaseUrl = oc.toFileUrl(path.resolve(__dirname, "../../openshift"));
 
   // The building of your cool app goes here ▼▼▼
-
+  objects = objects.concat(
+    oc.processDeploymentTemplate(`${templatesLocalBaseUrl}/bc.yaml`, {
+      param: {
+        NAME: phases[phase].name,
+        SUFFIX: phases[phase].suffix,
+        SOURCE_REPOSITORY_URL: oc.git.http_url,
+        SOURCE_REPOSITORY_REF: oc.git.ref,
+      },
+    }),
+  );
   oc.applyRecommendedLabels(
     objects,
     phases[phase].name,
--- api/.pipeline/lib/deploy.js
+++ api/.pipeline/lib/deploy.js
@@ -13,7 +13,17 @@ module.exports = settings => {
   var objects = [];
 
   // The deployment of your cool app goes here ▼▼▼
-
+  objects = objects.concat(
+    oc.processDeploymentTemplate(`${templatesLocalBaseUrl}/dc.yaml`, {
+      param: {
+        NAME: phases[phase].name,
+        SUFFIX: phases[phase].suffix,
+        SOURCE_REPOSITORY_URL: oc.git.http_url,
+        SOURCE_REPOSITORY_REF: oc.git.ref,
+      },
+    }),
+  );
+  
   oc.applyRecommendedLabels(
     objects,
     phases[phase].name,
