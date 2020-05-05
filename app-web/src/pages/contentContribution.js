263b3c800c3b397ca543868b6d59afbd97f640d0
jas29 <karansaini29@gmail.com>
2020-05-05 11:08:22 -0700

    Add auth

--- app-web/src/pages/contentContribution.js
+++ app-web/src/pages/contentContribution.js
@@ -1,42 +1,40 @@
 import React, { useState } from 'react';
 import Layout from '../hoc/Layout';
+import { useKeycloak } from '@react-keycloak/web';
 
 // components
 import { SEO } from '../components/SEO/SEO';
 import { Title } from '../components/Page';
 import Main from '../components/Page/Main';
-import { SelectDropdown } from '../components/Form/form';
 import TopicForm from '../components/Form/TopicForm';
 
 const contentContribution = () => {
-  const [currentFormValue, setCurrentFormValue] = useState('');
-  let content;
-   if (currentFormValue === 'addNewTopic') {
-    content = <TopicForm></TopicForm>;
+  const [keycloak] = useKeycloak();
+  const isAuthenticated = keycloak && keycloak.authenticated;
+  if (isAuthenticated === false){
+    return(
+      <Layout>
+        <SEO title="Content Contribution"></SEO>
+        <Main>
+          <Title title="Content Contribution" subtitle="Login via IDIR or Github to continue"/>
+        </Main>
+      </Layout>
+    )
   }
-  return (
-    <Layout>
-      <SEO title="Content Contribution" />
-      <Main>
-        <Title
-          title="Content Contribution"
-          subtitle="Want to add content to the Devhub? Let's answer some questions first"
-        />
-        <form>
-          <SelectDropdown
-            label="How would you like to contribute ?"
-            onChange={e => {
-              setCurrentFormValue(e.target.value);
-            }}
-          >
-            <option value="">Select a job type</option>
-            <option value="addNewTopic">Add a new Topic</option>
-          </SelectDropdown>
-        </form>
-        {content}
-      </Main>
-    </Layout>
-  );
+  else{
+    return (
+      <Layout>
+        <SEO title="Content Contribution" />
+        <Main>
+          <Title
+            title="Content Contribution"
+            subtitle="Add a topic to the Devhub"
+          />
+          <TopicForm></TopicForm>
+        </Main>
+      </Layout>
+    );
+   }
 };
 
 export default contentContribution;
