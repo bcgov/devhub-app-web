0e87f2b8b0211e5fa980d92adc8979564e6440b6
jas29 <karansaini29@gmail.com>
2020-07-10 15:44:29 -0700

    enable auth

--- api/src/index.js
+++ api/src/index.js
@@ -68,7 +68,7 @@ app.use(cors({ origin: originIsWhitelisted }));
 // authenticated routes
 
 authmware(app);
-// app.use(passport.authenticate('jwt', { session: false }));
+app.use(passport.authenticate('jwt', { session: false }));
 // app.options('/v1/topics', (req, res) => {
 
 // })
