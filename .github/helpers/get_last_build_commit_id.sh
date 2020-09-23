130bd95f4f72e2ab4319979b330930126936fcc7
Patrick Simonian <patricksimonian@gmail.com>
2020-09-23 12:04:15 -0700

    fix oopsy

--- .github/helpers/get_last_build_commit_id.sh
+++ .github/helpers/get_last_build_commit_id.sh
@@ -9,7 +9,7 @@
 INFRA_NAME=devhub-app-we34b
 SUFFIX=-146e7
 
-LATEST_BUILD=$(oc get -n $TOOLS_NAMESPACE build -l buildconfig=$INFRA_NAME$SUFFIX --sort-by='{.metadata.creationTimestamp}' -o name | tail -n 1 -r)
+LATEST_BUILD=$(oc get -n $TOOLS_NAMESPACE build -l buildconfig=$INFRA_NAME$SUFFIX --sort-by='{.metadata.creationTimestamp}' -o name | tail -n 1)
 
 if [ "$LATEST_BUILD" = "No resources found" ] || [  -z "$LATEST_BUILD" ]; then
   echo "No Builds Found"
