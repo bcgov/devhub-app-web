108920b036777e6ffd0004ca7a002d9fba61c1e1
Patrick Simonian <patricksimonian@gmail.com>
2020-09-23 11:47:56 -0700

    refine workflow

---
+++ .github/helpers/get_last_build_commit_id.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# returns the last commit used during a build or 'No Builds Found' if no build exists
+# environment variables
+# INFRA_NAME
+# SUFFIX
+
+INFRA_NAME=devhub-app-we34b
+SUFFIX=-146e7
+
+LATEST_BUILD=$(oc get build -l buildconfig=$INFRA_NAME$SUFFIX --sort-by='{.metadata.creationTimestamp}' -o name | tail -n 1 -r)
+
+if [ "$LATEST_BUILD" = "No resources found" ] || [  -z "$LATEST_BUILD" ]; then
+  echo "No Builds Found"
+  exit 0
+fi
+
+LATEST_COMMIT=$(oc get ${LATEST_BUILD} -o json | jq '.spec.revision.git.commit')
+
+echo $LATEST_COMMIT
