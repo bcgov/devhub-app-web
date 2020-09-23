6c0fe853564722d04157d3a742cdfc597e8ba0eb
Patrick Simonian <patricksimonian@gmail.com>
2020-09-23 12:07:01 -0700

    again

--- .github/helpers/get_last_build_commit_id.sh
+++ .github/helpers/get_last_build_commit_id.sh
@@ -10,7 +10,7 @@ INFRA_NAME=devhub-app-we34b
 SUFFIX=-146e7
 
 LATEST_BUILD=$(oc get -n $TOOLS_NAMESPACE build -l buildconfig=$INFRA_NAME$SUFFIX --sort-by='{.metadata.creationTimestamp}' -o name | tail -n 1)
-
+echo $LATEST_BUILD FUCK MY LIFE
 if [ "$LATEST_BUILD" = "No resources found" ] || [  -z "$LATEST_BUILD" ]; then
   echo "No Builds Found"
   exit 0
