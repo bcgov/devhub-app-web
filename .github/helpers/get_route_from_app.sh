476b91f376e22d8784a2d1702723b942fbbd0663
Patrick Simonian <patricksimonian@gmail.com>
2020-09-25 16:33:49 -0700

    improve route script

--- .github/helpers/get_route_from_app.sh
+++ .github/helpers/get_route_from_app.sh
@@ -10,5 +10,7 @@
 
 _ROUTE=$(oc -n $NAMESPACE get route/$INFRA_NAME$SUFFIX -o json)
 _HOST=$(echo $_ROUTE | jq '.spec.host')
-
+# remove leading and trailing quotes
+_HOST="${HOST%\"}"
+_HOST="${HOST#\"}"
 echo $_HOST
