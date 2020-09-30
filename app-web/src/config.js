d9e9de4f9df32f21715f04840985366b2bf3341f
Patrick Simonian <patricksimonian@gmail.com>
2020-09-30 14:55:56 -0700

    add caddy templates

--- app-web/src/config.js
+++ app-web/src/config.js
@@ -1,9 +1,9 @@
 // {{....}} is a Caddy template string, this will be replaced by caddy at run time
 export default {
-  ssoRealmName: '{{.Env.SSO_REALM_NAME}}',
-  ssoClientId: '{{.Env.SSO_CLIENT_ID}}',
-  ssoBaseUrl: '{{.Env.SSO_BASE_URL}}',
-  searchgateApiUrl: '{{.Env.SEARCHGATE_API_URL}}',
-  algoliaIndexSuffix: '{{.Env.ALGOLIA_INDEX_NAME_SUFFIX}}',
-  devhubApiUrl: '{{.Env.DEVHUB_API_URL}}',
+  ssoRealmName: '{{env "SSO_REALM_NAME"}}',
+  ssoClientId: '{{env "SSO_CLIENT_ID"}}',
+  ssoBaseUrl: '{{env "SSO_BASE_URL"}}',
+  searchgateApiUrl: '{{env "SEARCHGATE_API_URL"}}',
+  algoliaIndexSuffix: '{{env "ALGOLIA_INDEX_NAME_SUFFIX"}}',
+  devhubApiUrl: '{{env "DEVHUB_API_URL"}}',
 };
