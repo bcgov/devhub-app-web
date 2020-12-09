400b82883ee2de47c49e4d6d7ae0154465010cb2
Andrew Jun <97andrewjun@gmail.com>
2020-12-09 10:59:37 -0800

    Changed SSO urls in pipeline for web and api

--- api/.pipeline/lib/deploy.js
+++ api/.pipeline/lib/deploy.js
@@ -15,7 +15,7 @@ const ENVS = {
  */
 const getParamsByEnv = (env, pr) => {
   const params = {
-    SSO_BASE_URL_VALUE: 'https://sso.pathfinder.gov.bc.ca',
+    SSO_BASE_URL_VALUE: 'https://oidc.gov.bc.ca',
     SSO_CLIENT_ID_VALUE: 'devhub-api',
     SSO_REALM_NAME_VALUE: 'devhub',
     CORS_ORIGIN: 'https://developer.gov.bc.ca',
@@ -28,19 +28,19 @@ const getParamsByEnv = (env, pr) => {
       return {
         ...params,
         VERSION: 'test',
-        SSO_BASE_URL_VALUE: 'https://sso-test.pathfinder.gov.bc.ca',
+        SSO_BASE_URL_VALUE: 'https://test.oidc.gov.bc.ca',
         CORS_ORIGIN: 'https://devhub-static-test-devhub-test.pathfinder.gov.bc.ca/',
       };
     case ENVS.DEV:
       return {
         ...params,
-        SSO_BASE_URL_VALUE: 'https://sso-dev.pathfinder.gov.bc.ca',
+        SSO_BASE_URL_VALUE: 'https://dev.oidc.gov.bc.ca',
         SSO_CLIENT_ID_VALUE: `devhub-api-${pr}`,
       };
     default:
       return {
         ...params,
-        SSO_BASE_URL_VALUE: 'https://sso-dev.pathfinder.gov.bc.ca',
+        SSO_BASE_URL_VALUE: 'https://dev.oidc.gov.bc.ca',
         SSO_CLIENT_ID_VALUE: `devhub-api-${pr}`,
       };
   }
