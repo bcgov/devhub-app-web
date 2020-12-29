8cc11ca0215ad6af9478e06446cf779f8e81792e
Patrick Simonian <patricksimonian@gmail.com>
2020-12-29 10:08:18 -0800

    fix another oopsy

--- .github/helpers/clone_algolia_index.sh
+++ .github/helpers/clone_algolia_index.sh
@@ -7,6 +7,6 @@
 
 curl -X POST \
      -H "X-Algolia-API-Key: ${ALGOLIA_API_KEY}" \
-     -H "X-Algolia-Application-Id: ${ALGOLIA_APPLICATION_ID}" \
+     -H "X-Algolia-Application-Id: ${ALGOLIA_APP_ID}" \
      --data-binary "{ \"operation\": \"copy\", \"destination\":\"${ALOGOLIA_DEST_INDEX}\" }" \
-    "https://${ALGOLIA_APPLICATION_ID}.algolia.net/1/indexes/${ALGOLIA_SOURCE_INDEX}/operation"
+    "https://${ALGOLIA_APP_ID}.algolia.net/1/indexes/${ALGOLIA_SOURCE_INDEX}/operation"
