2d1f40101909bcf3227c746be363776d183f0ef9
Patrick Simonian <patricksimonian@gmail.com>
2020-12-29 10:18:07 -0800

    debug

--- .github/helpers/clone_algolia_index.sh
+++ .github/helpers/clone_algolia_index.sh
@@ -4,6 +4,7 @@
 # ALGOLIA_APP_ID string
 # ALGOLIA_SOURCE_INDEX string
 # ALGOLIA_DEST_INDEX string
+echo https://${ALGOLIA_APP_ID}.algolia.net/1/indexes/${ALGOLIA_SOURCE_INDEX}/operation"
 
 curl -X POST \
      -H "X-Algolia-API-Key: ${ALGOLIA_API_KEY}" \
