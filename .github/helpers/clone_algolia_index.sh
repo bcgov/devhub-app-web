2385f58f0cec0df14501a49bcd82623e69a25cfa
Patrick Simonian <Patrick.Simonian@gov.bc.ca>
2020-12-29 09:07:49 -0800

    include algolia index cloning (#1528)
    * include algolia index cloning
    
    * Apply suggestions from code review

---
+++ .github/helpers/clone_algolia_index.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+# variables
+# ALGOLIA_API_KEY string
+# ALGOLIA_APP_ID string
+# ALGOLIA_SOURCE_INDEX string
+# ALGOLIA_DEST_INDEX string
+
+curl -X POST \
+     -H "X-Algolia-API-Key: ${ALGOLIA_API_KEY}" \
+     -H "X-Algolia-Application-Id: ${ALGOLIA_APPLICATION_ID}" \
+     --data-binary "{ \"operation\": \"copy\", \"destination\":\"${ALOGOLIA_DEST_INDEX}\" }" \
+    "https://${APPLICATION_ID}.algolia.net/1/indexes/${ALGOLIA_SOURCE_INDEX}/operation"
