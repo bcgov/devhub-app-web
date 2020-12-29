d501d4099b1d40945a4158eb73bbc4e662e2eb98
Patrick Simonian <patricksimonian@gmail.com>
2020-12-29 15:27:41 -0800

    fix up typo

--- .github/helpers/clone_algolia_index.sh
+++ .github/helpers/clone_algolia_index.sh
@@ -4,11 +4,9 @@
 # ALGOLIA_APP_ID string
 # ALGOLIA_SOURCE_INDEX string
 # ALGOLIA_DEST_INDEX string
-echo "https://${ALGOLIA_APP_ID}.algolia.net/1/indexes/${ALGOLIA_SOURCE_INDEX}/operation"
 
 curl -X POST \
      -H "X-Algolia-API-Key: ${ALGOLIA_API_KEY}" \
      -H "X-Algolia-Application-Id: ${ALGOLIA_APP_ID}" \
-     --data-binary "{ \"operation\": \"copy\", \"destination\":\"${ALOGOLIA_DEST_INDEX}\" }" \
-     --fail \
+     --data-binary "{ \"operation\": \"copy\", \"destination\":\"${ALGOLIA_DEST_INDEX}\" }" \
     "https://${ALGOLIA_APP_ID}.algolia.net/1/indexes/${ALGOLIA_SOURCE_INDEX}/operation"
