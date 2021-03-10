14a67749b27b84c81b958d4532a2ca101970db06
Patrick Simonian <Patrick.Simonian@gov.bc.ca>
2021-03-10 10:08:19 -0800

    fix algolia bug (#1577)
    * fix algolia bug
    
    * split up workflows

--- .github/helpers/clone_algolia_index.sh
+++ .github/helpers/clone_algolia_index.sh
@@ -4,8 +4,12 @@
 # ALGOLIA_APP_ID string
 # ALGOLIA_SOURCE_INDEX string
 # ALGOLIA_DEST_INDEX string
+
+echo "Copying ${ALGOLIA_SOURCE_INDEX} to ${ALGOLIA_DEST_INDEX}"
 curl -X POST \
      -H "X-Algolia-API-Key: ${ALGOLIA_API_KEY}" \
      -H "X-Algolia-Application-Id: ${ALGOLIA_APP_ID}" \
      --data-binary "{ \"operation\": \"copy\", \"destination\":\"${ALGOLIA_DEST_INDEX}\" }" \
     "https://${ALGOLIA_APP_ID}.algolia.net/1/indexes/${ALGOLIA_SOURCE_INDEX}/operation"
+
+
