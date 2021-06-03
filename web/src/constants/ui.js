bcdaf645508924361875189046f0e4d46168a5f7
Andrew Jun <97andrewjun@gmail.com>
2021-06-03 11:22:44 -0700

    add ocp4 service overview to featured content

--- web/src/constants/ui.js
+++ web/src/constants/ui.js
@@ -91,6 +91,7 @@ export const DYNAMIC_TOPIC_PATHS = {
 // an expressed in order by this object
 // this is a lookup table to easily identify which resources are considered featured
 export const FEATURED_CONTENT = [
+  'Service Overview: Private Cloud Platform as a Service (aka DevOps Openshift 4 Platform)',
   'Exchange Lab Ops Learning Programs',
   'BCDevExchange Platform Status Page',
   'BC Developers Exchange',
