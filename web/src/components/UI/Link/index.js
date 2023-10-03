f126eb41a9b6618107623762c1f735d871e8cf6b
Shea Phillips <shea.phillips@gmail.com>
2023-10-03 14:04:02 -0700

    DEVX-75 adding banner to point to MVP site.

--- web/src/components/UI/Link/index.js
+++ web/src/components/UI/Link/index.js
@@ -1,4 +1,5 @@
 import Link from './Link';
 import SearchAwareLink from './SearchAwareLink';
 import ChevronLink from './ChevronLink';
-export { Link, SearchAwareLink, ChevronLink };
+import ButtonLink from './ButtonLink';
+export { Link, SearchAwareLink, ChevronLink, ButtonLink };
